7c1633890b23028cd401e9169e0096732af4b96a
Davyd McColl <davyd.mccoll@chillisoft.co.za>
2014-05-05 23:19:29 +0200

    * foo

--- PeanutButter/PeanutButter.XmlUtils.Tests/TestXElementExtensions.cs
+++ PeanutButter/PeanutButter.XmlUtils.Tests/TestXElementExtensions.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using System.Xml.Linq;
 using NUnit.Framework;
 using PeanutButter.RandomGenerators;
-using PeanutButter.Utils;
 
-namespace PenautButter.Utils.Tests
+namespace PeanutButter.XmlUtils.Tests
 {
     [TestFixture]
     class TestXElementExtensions
--- PeanutButter/PeanutButter.XmlUtils/XElementExtensions.cs
+++ PeanutButter/PeanutButter.XmlUtils/XElementExtensions.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Linq;
 
-namespace PeanutButter.Utils
+namespace PeanutButter.XmlUtils
 {
     public static class XElementExtensions
     {
