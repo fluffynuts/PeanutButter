7d66b752eae916712662b1c5d5210f45a686e8b6
Davyd McColl <davyd.mccoll@chillisoft.co.za>
2015-10-13 14:19:05 +0200

    * include ExpressionHelper in PeanutButter.Utils so I can use     it from another project     - use ExpressionHelper to resolve property paths from         lambda expressions into strings so you can do interesting         things with them * version bump for nuget release

---
+++ PeanutButter/PeanutButter.Utils.Tests/TestExpressionHelper.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using PeanutButter.Utils;
+
+namespace PenautButter.Utils.Tests
+{
+    [TestFixture]
+    public class TestExpressionHelper
+    {
+        public class SomeThing
+        {
+            public string StringProperty { get; set; }
+            public SomeThing Child { get; set; }
+        }
+
+        [Test]
+        public void GetMemberPathFor_ShouldReturnPathToImmediateMemberFromExpression()
+        {
+            //---------------Set up test pack-------------------
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = ExpressionUtil.GetMemberPathFor<SomeThing>(o => o.StringProperty);
+
+            //---------------Test Result -----------------------
+            Assert.AreEqual("StringProperty", result);
+        }
+
+        [Test]
+        public void GetMemberPathFor_ShouldBeAbleToResolveChildPaths()
+        {
+            //---------------Set up test pack-------------------
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = ExpressionUtil.GetMemberPathFor<SomeThing>(o => o.Child.StringProperty);
+
+            //---------------Test Result -----------------------
+            Assert.AreEqual("Child.StringProperty", result);
+        }
+
+
+        [Test]
+        public void GetPropertyTypeFor_ShouldReturnTypeOfPropertyReferencedByExpression()
+        {
+            //---------------Set up test pack-------------------
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var result = ExpressionUtil.GetPropertyTypeFor<SomeThing>(o => o.StringProperty);
+
+            //---------------Test Result -----------------------
+            Assert.AreEqual(typeof(string), result);
+        }
+
+
+    }
+}
---
+++ PeanutButter/PeanutButter.Utils/ExpressionHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace PeanutButter.Utils
+{
+    public class ExpressionUtil
+    {
+        public static string GetMemberPathFor<TSource>(Expression<Func<TSource, object>> expression)
+        {
+            var memberExpression = expression.Body as MemberExpression;
+            if (memberExpression == null)
+            {
+                var unaryExpression = expression.Body as UnaryExpression;
+                if (unaryExpression == null) throw new Exception("expression not supported");
+                memberExpression = unaryExpression.Operand as MemberExpression;
+                if (memberExpression == null)
+                {
+                    // try for a method
+                    var instanceMethodExpression = unaryExpression.Operand as MethodCallExpression;
+                    if (instanceMethodExpression != null)
+                    {
+                        // TODO: make this able to traverse levels like with properties
+                        return instanceMethodExpression.Method.Name;
+                    }
+                    throw new Exception("expression is not a member expression");
+                }
+            }
+            return GetFullPropertyPathNameFrom(memberExpression);
+        }
+
+        public static Type GetPropertyTypeFor<TSource>(Expression<Func<TSource, object>> expression)
+        {
+            var propertyPath = GetMemberPathFor(expression);
+            var pathParts = new Stack<string>(propertyPath.Split(new[] {"."}, StringSplitOptions.None));
+            var result = typeof(TSource);
+            do
+            {
+                var part = pathParts.Pop();
+                var propInfo = result.GetProperty(part);
+                result = propInfo.PropertyType;
+            } while (pathParts.Any());
+            return result;
+        }
+
+        public static string GetFullPropertyPathNameFrom(MemberExpression expression)
+        {
+            var parts = new List<string>(new[] {expression.Member.Name});
+            var next = expression.Expression as MemberExpression;
+            while (next != null)
+            {
+                parts.Add(next.Member.Name);
+                next = next.Expression as MemberExpression;
+            }
+            parts.Reverse();
+            return string.Join(".", parts);
+        }
+    }
+}
