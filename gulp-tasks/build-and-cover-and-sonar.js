e294d8902e7378bc5d70938e2c32834adc59a272
Davyd McColl <davyd.mccoll@chillisoft.co.za>
2016-01-04 17:18:47 +0200

    * moo

--- gulp-tasks/build-and-cover-and-sonar.js
+++ gulp-tasks/build-and-cover-and-sonar.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var runSequence = require('run-sequence');
 
-gulp.task('default', function(cb) {
+gulp.task('build-and-cover-and-sonar', function(cb) {
     runSequence('build', 'cover', 'sonar-only', function(err) {
         if (err) {
             console.log(err);
