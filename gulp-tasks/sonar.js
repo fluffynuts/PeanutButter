fd7404f1ee763cfd45681afaaab9e5bc8c539a79
Davyd McColl <davyd.mccoll@chillisoft.co.za>
2016-01-05 08:53:18 +0200

    * try to get cleaner output printed from sonar error

--- gulp-tasks/sonar.js
+++ gulp-tasks/sonar.js
@@ -7,6 +7,8 @@ gulp.task('sonar', ['cover'], function(done) {
     exec('C:/sonar/sonar-runner-2.4/bin/sonar-runner.bat').then(function() {
         done();
     }).catch(function(err) {
-        log.error('Sonar fails: ' + err.code);   
+        var errorOutput = (err && err.error) ? err.error.stderr : '(no stderr output?)';
+        log.error('Sonar fails! Output follows:');   
+        log.error(errorOutput.replace('\\r', '\r').replace('\\n', '\n'));
     });
 });
