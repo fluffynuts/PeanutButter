6dbd54d72617ae925482dad909052f48e31a3093
Davyd McColl <davyd.mccoll@chillisoft.co.za>
2016-01-12 11:31:28 +0200

    * implement expected naming conventions such that TinyEventAggregator     tests will actually be run at CI

--- PeanutButter/PeanutButter.TinyEventAggregator.Tests/TestEventBase.cs
+++ PeanutButter/PeanutButter.TinyEventAggregator.Tests/TestEventBase.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NUnit.Framework;
-using PeanutButter.TinyEventAggregator;
 
-namespace TinyEventAggregator.Tests
+namespace PeanutButter.TinyEventAggregator.Tests
 {
     [TestFixture]
     public class TestEventBase
--- PeanutButter/PeanutButter.TinyEventAggregator.Tests/TestTinyEventAggregator.cs
+++ PeanutButter/PeanutButter.TinyEventAggregator.Tests/TestTinyEventAggregator.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
-using PeanutButter.TinyEventAggregator;
+﻿using NUnit.Framework;
 
-namespace TinyEventAggregator.Tests
+namespace PeanutButter.TinyEventAggregator.Tests
 {
     [TestFixture]
     public class TestTinyEventAggregator
