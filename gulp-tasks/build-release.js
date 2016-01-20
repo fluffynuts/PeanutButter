feb00d925eb7bb5d0ccd12be2a4a2d25c89b3c99
Davyd McColl <davyd.mccoll@chillisoft.co.za>
2016-01-20 10:09:12 +0200

    * add build-release target     - should build and push packages, will test at next release

---
+++ gulp-tasks/build-release.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var msbuild = require('gulp-msbuild');
+gulp.task('build-release', function() {
+    return gulp.src('**/*.sln')
+            .pipe(msbuild({
+                errorOnFail: true,
+                toolsVersion: 14.0,  // use 12.0 rather? 4.0 is supported by Mono...
+                targets: ['Clean', 'Build'],
+                configuration: 'Release',
+                stdout: true,
+                verbosity: 'minimal',
+                architecture: 'x64'
+            }));
+});
+
+
