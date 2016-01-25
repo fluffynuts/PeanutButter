15760d066443e37e44059f7a68593e354836c52d
Davyd McColl <davyd.mccoll@chillisoft.co.za>
2016-01-25 14:37:54 +0200

    * add testing around PortUnavailableException

---
+++ PeanutButter/PeanutButter.SimpleTcpServer.Tests/TestPortUnavailableException.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+using PeanutButter.RandomGenerators;
+using PeanutButter.TestUtils.Generic;
+
+namespace PeanutButter.SimpleTcpServer.Tests
+{
+    [TestFixture]
+    public class TestPortUnavailableException
+    {
+        [Test]
+        public void Type_ShouldInheritFrom_Exception()
+        {
+            //---------------Set up test pack-------------------
+            var sut = typeof (PortUnavailableException);
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            sut.ShouldInheritFrom<Exception>();
+
+            //---------------Test Result -----------------------
+        }
+
+        [Test]
+        public void Construct_GivenPort_ShouldSetMessage()
+        {
+            //---------------Set up test pack-------------------
+            var port = RandomValueGen.GetRandomInt();
+            var expected = "Can't listen on specified port '" + port + "': probably already in use?";
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var sut = new PortUnavailableException(port);
+            var result = sut.Message;
+
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, result);
+        }
+
+
+    }
+}
