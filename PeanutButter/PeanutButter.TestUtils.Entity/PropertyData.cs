eba3096f6e157608e0bc7234fefc6aa2889c510a
Davyd McColl <davyd.mccoll@chillisoft.co.za>
2016-01-25 14:50:24 +0200

    * remove static helper For * rename PropertyDataGeneric

--- PeanutButter/PeanutButter.TestUtils.Entity/PropertyData.cs
+++ PeanutButter/PeanutButter.TestUtils.Entity/PropertyData.cs
@@ -1,14 +1,30 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
+using PeanutButter.Utils;
 
 namespace PeanutButter.TestUtils.Entity
 {
-    public static class PropertyData
+    public class PropertyData<T>
     {
-        public static PropertyData<T> For<T>(T item, Expression<Func<T, object>> propertyExpression)
+        public string PropertyPath { get; private set; }
+        public PropertyInfo PropertyInfo { get; private set; }
+        public IEnumerable<Attribute> CustomAttributes { get; private set; }
+        public Type ParentType { get; private set; }
+
+        public PropertyData(T item, Expression<Func<T, object>> propertyExpression)
         {
-            return new PropertyData<T>(item, propertyExpression);
+            PropertyPath = ExpressionUtil.GetMemberPathFor(propertyExpression);
+            PropertyInfo = item.GetPropertyInfoFor(PropertyPath);
+            CustomAttributes = PropertyInfo.GetCustomAttributes();
+            ParentType = typeof(T);
+        }
+
+        public TAttribute CustomAttribute<TAttribute>() where TAttribute: Attribute
+        {
+            return CustomAttributes.OfType<TAttribute>().FirstOrDefault();
         }
     }
-
 }
