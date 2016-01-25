1c22319ad8f234e00d0c152aee190f7d83015751
Davyd McColl <davyd.mccoll@chillisoft.co.za>
2016-01-25 13:15:16 +0200

    * test ServiceUnconfiguredException

---
+++ PeanutButter/PeanutButter.ServiceShell.Tests/TestServiceUnconfiguredException.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PeanutButter.RandomGenerators;
+using PeanutButter.TestUtils.Generic;
+
+namespace PeanutButter.ServiceShell.Tests
+{
+    [TestFixture]
+    public class TestServiceUnconfiguredException
+    {
+        [Test]
+        public void Type_ShouldInheritFrom_Exception()
+        {
+            //---------------Set up test pack-------------------
+            var sut = typeof (ServiceUnconfiguredException);
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            sut.ShouldInheritFrom<Exception>();
+
+            //---------------Test Result -----------------------
+        }
+
+        [Test]
+        public void Construct_ShouldSetMessageContainingProvidedPropertyName()
+        {
+            //---------------Set up test pack-------------------
+            var property = RandomValueGen.GetRandomString();
+            var expected = "This service is not completely configured. Please set the " + property + " property value.";
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var sut = new ServiceUnconfiguredException(property);
+
+            //---------------Test Result -----------------------
+            Assert.AreEqual(expected, sut.Message);
+        }
+
+
+    }
+}
