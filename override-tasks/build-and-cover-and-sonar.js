d62818a5354a96a63d78abd14b579d94e933199e
Davyd McColl <davyd.mccoll@chillisoft.co.za>
2016-01-27 16:31:09 +0200

    * underp jobs after importing from remote gulp-tasks

--- override-tasks/build-and-cover-and-sonar.js
+++ override-tasks/build-and-cover-and-sonar.js
@@ -2,8 +2,8 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var runSequence = require('run-sequence');
 
-gulp.task('default', function(cb) {
-    runSequence('purge', 'git-submodules', 'build', 'test-dotnet', function(err) {
+gulp.task('build-and-cover-and-sonar', function(cb) {
+    runSequence('purge', 'git-submodules', 'build', 'cover-dotnet', 'sonar', function(err) {
         if (err) {
             gutil.log(gutil.colors.red(gutil.colors.bold(err)));
         }
