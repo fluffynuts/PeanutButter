fed3abcb123059801bbd3390e190cc9062c7cb97
Davyd McColl <davyd.mccoll@chillisoft.co.za>
2016-03-02 11:11:44 +0200

    1.2.5 package release

--- source/Utils/PeanutButter.Utils/PropertyNotFoundException.cs
+++ source/Utils/PeanutButter.Utils/PropertyNotFoundException.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace PeanutButter.Utils
 {
-    internal class PropertyNotFoundException : Exception
+    public class PropertyNotFoundException : Exception
     {
         public PropertyNotFoundException(Type type, string propertyName):
             base("Property '" + propertyName + "' not found on type '" + type.Name + "'")
