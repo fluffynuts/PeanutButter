fa9e088625868a36ab25309f428d6fe28d53055e
Davyd McColl <davyd.mccoll@chillisoft.co.za>
2016-04-06 10:31:31 +0200

    * add testing around dto class RequestLogItem

---
+++ source/SimpleServers/PeanutButter.SimpleHTTPServer.Tests/TestRequestLogItem.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using static PeanutButter.RandomGenerators.RandomValueGen;
+
+namespace PeanutButter.SimpleHTTPServer.Tests
+{
+    [TestFixture]
+    public class TestRequestLogItem
+    {
+        [Test]
+        public void Construct_ShouldCopyParametersToProperties()
+        {
+            //---------------Set up test pack-------------------
+            var path = GetRandomString();
+            var code = GetRandom<HttpStatusCode>();
+            var message = GetRandomString();
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var sut = new RequestLogItem(path, code, message);
+
+            //---------------Test Result -----------------------
+            Assert.AreEqual(path, sut.Path);
+            Assert.AreEqual(code, sut.StatusCode);
+            Assert.AreEqual(message, sut.Message);
+        }
+
+        [Test]
+        public void Construct_GivenNullMessage_ShouldSetMessageFromStatusCodeString()
+        {
+            //---------------Set up test pack-------------------
+            var path = GetRandomString();
+            var code = GetRandom<HttpStatusCode>();
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            var sut = new RequestLogItem(path, code, null);
+
+            //---------------Test Result -----------------------
+            Assert.AreEqual(path, sut.Path);
+            Assert.AreEqual(code, sut.StatusCode);
+            Assert.AreEqual(code.ToString(), sut.Message);
+        }
+
+    }
+}
--- source/SimpleServers/PeanutButter.SimpleHTTPServer/RequestLogItem.cs
+++ source/SimpleServers/PeanutButter.SimpleHTTPServer/RequestLogItem.cs
@@ -4,9 +4,9 @@ namespace PeanutButter.SimpleHTTPServer
 {
     public class RequestLogItem
     {
-        public string Path { get; private set; }
-        public HttpStatusCode StatusCode { get; private set; }
-        public string Message { get; private set; }
+        public string Path { get; }
+        public HttpStatusCode StatusCode { get; }
+        public string Message { get; }
         public RequestLogItem(string path, HttpStatusCode code, string message)
         {
             Path = path;
