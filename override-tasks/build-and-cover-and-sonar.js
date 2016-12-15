e5e5ffb0b9055c862cb82dbb685e67f4eb644f69
Davyd McColl <davyd.mccoll@chillisoft.co.za>
2016-12-15 08:57:51 +0200

    chore: remove cover task from run sequence as sonar depends on it anyway

--- override-tasks/build-and-cover-and-sonar.js
+++ override-tasks/build-and-cover-and-sonar.js
@@ -3,7 +3,7 @@ var gutil = require('gulp-util');
 var runSequence = require('run-sequence');
 
 gulp.task('build-and-cover-and-sonar', function(cb) {
-    runSequence('purge', 'git-submodules', 'build', 'cover-dotnet', 'sonar', function(err) {
+    runSequence('purge', 'git-submodules', 'build', 'sonar', function(err) {
         if (err) {
             gutil.log(gutil.colors.red(gutil.colors.bold(err)));
         }
