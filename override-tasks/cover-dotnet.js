878add88e81595de634bdc23a6d4c2dcd044b4bd
Davyd McColl <davyd.mccoll@chillisoft.co.za>
2016-12-15 09:56:46 +0200

    chore: re-enable proper coverage :/

---
+++ override-tasks/cover-dotnet.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var dotCover = requireModule('gulp-dotcover');
+gulp.task('cover-dotnet', function() {
+    return gulp.src('**/*.Tests.dll')
+             .pipe(dotCover({
+                 debug: false,
+                 architecture: 'x86',
+                 exclude: ['FluentMigrator.*', 
+                            'AutoMapper',
+                            'AutoMapper.*']
+             }));
+});
+
