fe74200c8429d3d1f75b8df08232aa4aff5ebcea
Davyd McColl <davydm@gmail.com>
2017-05-10 10:32:05 +0200

    feat: track unified tools downloading logic from gulp-tasks

--- local-tasks/install-tools.js
+++ local-tasks/install-tools.js
@@ -1,53 +1,11 @@
 const
   gulp = requireModule("gulp-with-help"),
-  downloadNuget = requireModule("nuget-downloader");
-exec = requireModule("exec"),
-  path = require("path"),
-  fs = require("fs"),
-  toolsFolder = "tools",
-  nugetExe = "nuget.exe",
-  del = require("del"),
-  requiredTools = [
+  installLocalTools = requireModule("install-local-tools");
+
+gulp.task("install-tools", () => {
+  return installLocalTools([
     "nunit.console",
     "opencover",
     "reportgenerator"
-  ];
-
-gulp.task("make-tools-folder-if-missing", false, () => {
-  return new Promise((resolve, reject) => {
-    if (!fs.existsSync(toolsFolder)) {
-      fs.mkdirSync(toolsFolder);
-    }
-    resolve();
-  });
+  ]);
 });
-
-gulp.task("clean-tools", 
-  "Cleans out the tools folder of all tools", 
-  [ "make-tools-folder-if-missing" ],
-  () => {
-  const dirs = fs.readdirSync(toolsFolder)
-    .map(p => path.join(toolsFolder, p))
-    .filter(p => {
-      const stat = fs.lstatSync(p);
-      return stat.isDirectory();
-    });
-  return del(dirs);
-});
-
-gulp.task("update-tools-nuget", "Updates nuget.exe in the tools folder", () => {
-  return downloadNuget(toolsFolder);
-});
-
-gulp.task("install-tools",
-  "Installs required tools to /tools folder",
-  ["clean-tools", "update-tools-nuget"],
-  () => {
-    const
-      promises = requiredTools.map(tool => exec(
-        nugetExe,
-        ["install", tool],
-        { cwd: toolsFolder }
-      ));
-    return Promise.all(promises);
-  });
