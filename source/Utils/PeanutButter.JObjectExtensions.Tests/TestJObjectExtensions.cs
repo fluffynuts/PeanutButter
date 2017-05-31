460d3867a7b4941e92c7b135c59c1e8fee10a660
Davyd McColl <davydm@gmail.com>
2017-05-31 20:32:07 +0200

    feat: add PeanutButter.JObjectExtensions to make DuckTyping Newtonsoft JObjects less of a PITA

---
+++ source/Utils/PeanutButter.JObjectExtensions.Tests/TestJObjectExtensions.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+
+namespace PeanutButter.JObjectExtensions.Tests
+{
+    [TestFixture]
+    public class TestJObjectExtensions
+    {
+        [Test]
+        public void ToDictionary_ShouldConvertOneLevel()
+        {
+            // Arrange
+            var json = @"
+{
+    int: 1,
+    string: ""moo"",
+    bool: true,
+}
+";
+            var jobj = JsonConvert.DeserializeObject(json) as JObject;
+            // Pre-Assert
+
+            // Act
+            var result = jobj.ToDictionary();
+
+            // Assert
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result["int"]);
+            Assert.AreEqual("moo", result["string"]);
+            Assert.AreEqual(true, result["bool"]);
+        }
+
+        [Test]
+        public void ToDictionary_ShouldConvertTwoLevels()
+        {
+            // Arrange
+            var json = @"
+{
+    ""int"": 1,
+    ""string"": ""moo"",
+    ""bool"": true,
+    ""sub"": {
+        ""uri"": ""http://www.google.com"",
+        ""nullValue"": null
+    }
+}
+";
+            var jobj = JsonConvert.DeserializeObject(json) as JObject;
+            // Pre-Assert
+
+            // Act
+            var result = jobj.ToDictionary();
+
+            // Assert
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result["int"]);
+            Assert.AreEqual("moo", result["string"]);
+            Assert.AreEqual(true, result["bool"]);
+            Assert.IsNotNull(result["sub"]);
+            var sub = result["sub"] as Dictionary<string, object>;
+            Assert.AreEqual("http://www.google.com", sub["uri"]);
+            Assert.IsNull(sub["nullValue"]);
+        }
+
+        [Test]
+        public void ToDictionary_ShouldConvertArraysOfUniformType()
+        {
+            // Arrange
+            var json = @"
+{
+    ""int"": 1,
+    ""string"": ""moo"",
+    ""bool"": true,
+    ""sub"": {
+        ""uri"": ""http://www.google.com"",
+        ""nullValue"": null
+    },
+    ""collection"": [
+        ""cow"",
+        ""beef"",
+        ""bovine""
+    ]
+}
+";
+            var jobj = JsonConvert.DeserializeObject(json) as JObject;
+            // Pre-Assert
+
+            // Act
+            var result = jobj.ToDictionary();
+
+            // Assert
+
+            var collection = result["collection"] as string[];
+            Assert.IsNotNull(collection);
+            CollectionAssert.AreEqual(
+                new[] { "cow", "beef", "bovine" },
+                collection
+            );
+        }
+
+        [Test]
+        public void ToDictionary_ShouldConvertArraysOfNonUniformType()
+        {
+            // Arrange
+            var json = @"
+{
+    ""int"": 1,
+    ""string"": ""moo"",
+    ""bool"": true,
+    ""sub"": {
+        ""uri"": ""http://www.google.com"",
+        ""nullValue"": null
+    },
+    ""collection"": [
+        ""cow"",
+        43,
+        true
+    ]
+}
+";
+            var jobj = JsonConvert.DeserializeObject(json) as JObject;
+            // Pre-Assert
+
+            // Act
+            var result = jobj.ToDictionary();
+
+            // Assert
+
+            var collection = result["collection"] as object[];
+            Assert.IsNotNull(collection);
+            CollectionAssert.AreEqual(
+                new object[] { "cow", 43, true },
+                collection
+            );
+        }
+    }
+}
---
+++ source/Utils/PeanutButter.JObjectExtensions/JObjectExtensions.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Newtonsoft.Json.Linq;
+
+namespace PeanutButter.JObjectExtensions
+{
+    public static class JObjectExtensions
+    {
+        private static readonly JTokenResolvers _resolvers = new JTokenResolvers
+        {
+            {JTokenType.None, o => null},
+            {JTokenType.Array, ConvertJTokenArray},
+            {JTokenType.Property, ConvertJTokenProperty},
+            {JTokenType.Integer, o => o.Value<int>()},
+            {JTokenType.String, o => o.Value<string>()},
+            {JTokenType.Boolean, o => o.Value<bool>()},
+            {JTokenType.Null, o => null},
+            {JTokenType.Undefined, o => null},
+            {JTokenType.Date, o => o.Value<DateTime>()},
+            {JTokenType.Bytes, o => o.Value<byte[]>()},
+            {JTokenType.Guid, o => o.Value<Guid>()},
+            {JTokenType.Uri, o => o.Value<Uri>()},
+            {JTokenType.TimeSpan, o => o.Value<TimeSpan>()},
+            {JTokenType.Object, TryConvertObject}
+        };
+
+        // just because JsonCovert doesn't believe in using your provided type all the way down >_<
+        public static Dictionary<string, object> ToDictionary(this JObject src)
+        {
+            var result = new Dictionary<string, object>(StringComparer.OrdinalIgnoreCase);
+            if (src == null)
+                return result;
+            foreach (var prop in src.Properties())
+                result[prop.Name] = _resolvers[prop.Type](prop.Value);
+            return result;
+        }
+
+        private static object TryConvertObject(JToken arg)
+        {
+            var asJObject = arg as JObject;
+            if (asJObject != null)
+                return asJObject.ToDictionary();
+            return GetResolverFor(arg)(arg);
+        }
+
+        private static object PassThrough(JToken arg)
+        {
+            return arg;
+        }
+
+        private static Func<JToken, object> GetResolverFor(JToken arg)
+        {
+            Func<JToken, object> result;
+            return _resolvers.TryGetValue(arg.Type, out result)
+                ? result
+                : PassThrough;
+        }
+
+        private static object ConvertJTokenProperty(JToken arg)
+        {
+            var resolver = GetResolverFor(arg);
+            if (resolver == null)
+                throw new InvalidOperationException($"Unable to handle JToken of type: {arg.Type}");
+            return resolver(arg);
+        }
+
+        private static object ConvertJTokenArray(JToken arg)
+        {
+            var array = arg as JArray;
+            if (array == null)
+                throw new NotImplementedException();
+            var result = new List<object>();
+            foreach (var item in array)
+            {
+                result.Add(TryConvertObject(item));
+            }
+            var distinctType = FindSameTypeOf(result);
+            return distinctType == null
+                ? result.ToArray()
+                : ConvertToTypedArray(result, distinctType);
+        }
+
+        private static Type FindSameTypeOf(IEnumerable<object> src)
+        {
+            var types = src.Select(o => o.GetType()).Distinct().ToArray();
+            return types.Length == 1 ? types[0] : null;
+        }
+
+        private static object ConvertToTypedArray(IEnumerable<object> src, Type newType)
+        {
+            var method = ConvertToTypedArrayGenericMethod.MakeGenericMethod(newType);
+            return method.Invoke(null, new[] {src});
+        }
+
+        private static readonly MethodInfo ConvertToTypedArrayGenericMethod
+            = typeof(JObjectExtensions).GetMethod(
+                nameof(ConvertToTypedArrayGeneric),
+                BindingFlags.NonPublic | BindingFlags.Static
+            );
+
+        private static T[] ConvertToTypedArrayGeneric<T>(IEnumerable<object> src)
+        {
+            return src.Cast<T>().ToArray();
+        }
+
+        private class JTokenResolvers : Dictionary<JTokenType, Func<JToken, object>>
+        {
+        }
+    }
+}
