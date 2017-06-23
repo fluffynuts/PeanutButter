95bc6600430864c2df73f610c3d86fef0b392572
Davyd McColl <davydm@gmail.com>
2017-06-23 16:41:06 +0200

    wip: using PB to testbed pre:: and post:: hooks

--- gulpfile.js
+++ gulpfile.js
@@ -13,39 +13,55 @@
  *
  * To add or change tasks, do so one task per file in the gulp-tasks folder
  */
-var fs = require('fs'),
-    debug = require('debug')('gulpfile');
-var gulpTasksFolder = 'gulp-tasks'; // if you cloned elsewhere, you'll need to modify this
-global.requireModule = function(module) {
-    var modulePath = ['.', gulpTasksFolder, 'modules', module].join('/');
-    return require(modulePath);
+const 
+  fs = require("fs"),
+  path = require("path"),
+  debug = require("debug")("gulpfile"),
+  gulpTasksFolder = "gulp-tasks"; // if you cloned elsewhere, you"ll need to modify this
+global.requireModule = function (module) {
+  var modulePath = [".", gulpTasksFolder, "modules", module].join("/");
+  return require(modulePath);
 };
-var fs = require('fs');
-if (!fs.existsSync('package.json')) {
-    ['You\'re nearly there!',
-     'Please copy the package.json from the start folder alongside your gulpfile.js',
-     'then run `npm install` to install the required packages'].forEach(function(s) {
-        console.log(s);
-     });
-    process.exit(1);
-}
-try {
-    var requireDir = require('require-dir');
-    requireDir('gulp-tasks');
-    ['override-tasks', 'local-tasks'].forEach(function(dirname) {
-        if (fs.existsSync(dirname)) {
-            requireDir(dirname);
-        }
+
+if (!fs.existsSync("package.json")) {
+  ["You\"re nearly there!",
+    "Please copy the package.json from the start folder alongside your gulpfile.js",
+    "then run `npm install` to install the required packages"].forEach(function (s) {
+      console.log(s);
     });
-} catch (e) {
-    if (shouldDump(e)) {
-      console.error(e);
-    } else {
-      if (!process.env.DEBUG) {
-        console.log("Error occurred. For more info, set the DEBUG environment variable (eg set DEBUG=*).")
-      }
+  process.exit(1);
+}
+
+const 
+  generateTaskHooks = requireModule("generate-task-hooks"),
+  gulp = requireModule("gulp-with-help");
+
+try {
+  var requireDir = require("require-dir");
+  requireDir(gulpTasksFolder);
+  if (process.env.TEST_GULP_TASKS) {
+    console.warn("including tasks from tests folder");
+    requireDir(path.join(gulpTasksFolder, "tests"));
+  }
+  // WIP: Object.keys(gulp.tasks).forEach(generateTaskHooks); 
+  ["override-tasks", "local-tasks"].forEach(function (dirname) {
+    if (fs.existsSync(dirname)) {
+      requireDir(dirname);
     }
-    process.exit(1);
+  });
+} catch (e) {
+  if (shouldDump(e)) {
+    console.error(e);
+  } else {
+    if (!process.env.DEBUG) {
+      console.log([
+        "Error occurred.",
+        "For more info, set the DEBUG environment variable (eg set DEBUG=*).",
+        "If you still get nothing useful, set ALWAYS_DUMP_GULP_ERRORS and report the missed error as an issue"
+      ].join("\n"));
+    }
+  }
+  process.exit(1);
 }
 
 function shouldDump(e) {
@@ -55,10 +71,11 @@ function shouldDump(e) {
 function probablyNotReportedByGulp(e) {
   var message = (e || "").toString().toLowerCase();
   return [
-    "cannot find module",
-    "referenceerror",
-    "syntaxerror"
+    /cannot find module/i,
+    /referenceerror/i,
+    /syntaxerror/i,
+    /^Error:/i
   ].reduce((acc, cur) => {
-    return acc || message.indexOf(cur) > -1;
+    return acc || message.match(cur)
   }, false);
 }
