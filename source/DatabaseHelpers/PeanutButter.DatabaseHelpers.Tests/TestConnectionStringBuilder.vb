6064b0d640c46ceaf38167767590b087eb5d87e8
Davyd McColl <davydm@gmail.com>
2017-06-29 13:11:28 +0200

    sonar-chore: add some testing (low-hanging fruit)

---
+++ source/DatabaseHelpers/PeanutButter.DatabaseHelpers.Tests/TestConnectionStringBuilder.vb
@@ -0,0 +1,24 @@
+﻿Imports NUnit.Framework
+Imports PeanutButter.RandomGenerators
+
+<TestFixture()>
+Public Class TestConnectionStringBuilder
+    <Test()>
+    Public Sub Create_ShouldReturnNewBuilder()
+        Dim result = ConnectionStringBuilder.Create()
+        Assert.IsInstanceOf(Of ConnectionStringBuilder)(result)
+    End Sub
+
+    <Test()>
+    Public Sub Build_ShouldProduceExpectedConnectionString()
+        Dim provider = RandomValueGen.GetRandomString(4)
+        Dim source = RandomValueGen.GetRandomString(4)
+        Dim expected = "Provider=" & provider & ";Data Source=" & source
+
+        Dim result = ConnectionStringBuilder.Create() _
+                        .WithProvider(provider) _
+                        .WithSource(source) _
+                        .Build()
+        Assert.AreEqual(result, expected)
+    End Sub
+End Class
