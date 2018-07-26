99eb391d62e9ff743288ea64bf603c63aa7f2063
Davyd McColl <davydm@gmail.com>
2018-07-26 15:22:23 +0200

    chore: release version 1.2.240

---
+++ local-tasks/commit-release.js
@@ -0,0 +1,30 @@
+const gulp = requireModule("gulp-with-help"),
+  gutil = require("gulp-util"),
+  editXml = require("gulp-edit-xml"),
+  Git = require("simple-git"),
+  git = new Git(),
+  containingFolder = "source/Utils/PeanutButter.Utils"; // track PB.Utils for tag version
+
+gulp.task("commit-release", () => {
+  return new Promise((resolve, reject) => {
+    gulp.src(`${containingFolder}/Package.nuspec`).pipe(
+      editXml(xml => {
+        const node = xml.package.metadata[0].version,
+          version = node[0].trim();
+
+        gutil.log(gutil.colors.cyan(`Committing release ${version}`));
+        git.add("./*", err => {
+          if (err) {
+            reject(`Unable to add all files: ${err}`);
+          }
+          git.commit(`chore: release version ${version}`, err => {
+            return err
+              ? reject(`Unable to commit release ${version}: ${err}`)
+              : resolve(`Release ${version} committed`);
+          });
+        });
+        return xml;
+      })
+    );
+  });
+});
