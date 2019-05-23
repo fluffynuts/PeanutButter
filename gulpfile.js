e8e6694b28bb90dba0ff5b02e5ed7a6c553b0cd9
Davyd McColl <davydm@gmail.com>
2019-05-23 13:29:33 +0200

    :construction_worker: update gulp & track upstream gulp-tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,82 +1,162 @@
 /*
- * Welcome new user! To get started, ensure that you have copied
- * the start/packages.json alongside this file, in the root of your repo.
- * Then do:
- * > npm install
- * After that, you can:
- * > node node_modules/gulp/bin/gulp.js
- * OR, install gulp globally:
- * > npm install -g gulp
- * and then you can just:
- * > gulp
- * I HIGHLY recommend using the first method at your build server
- *
- * To add or change tasks, do so one task per file in the gulp-tasks folder
+  Welcome new user!
+
+  To get started, copy this gulpfile.js to the root of your repo and run:
+  `node gulpfile.js`
+  You should be guided through the basic setup. More information in README.md. In
+  particular, I highly recommend reading about how to use `local-tasks` to extend
+  and / or override the default task-set.
  */
-const 
+var 
+  os = require("os"),
   fs = require("fs"),
-  path = require("path"),
-  debug = require("debug")("gulpfile"),
-  gulpTasksFolder = "gulp-tasks"; // if you cloned elsewhere, you"ll need to modify this
-global.requireModule = function (module) {
-  var modulePath = [".", gulpTasksFolder, "modules", module].join("/");
-  return require(modulePath);
-};
+  gulpTasksFolder = "gulp-tasks", // if you cloned elsewhere, you"ll need to modify this
+  requireModule = global.requireModule = function(mod) {
+    var modulePath = [".", gulpTasksFolder, "modules", mod].join("/");
+    if (fs.existsSync(modulePath + ".js")) {
+        return require(modulePath);
+    } else {
+        return require(mod);
+    }
+  };
+
+
+if (!fs.existsSync(gulpTasksFolder)) {
+  console.error("Either clone `gulp-tasks` to the `gulp-tasks` folder or modify this script to avoid sadness");
+  process.exit(2);
+}
 
 if (!fs.existsSync("package.json")) {
-  ["You\"re nearly there!",
-    "Please copy the package.json from the start folder alongside your gulpfile.js",
-    "then run `npm install` to install the required packages"].forEach(function (s) {
-      console.log(s);
-    });
-  process.exit(1);
+  console.log(
+    "You need to set up a package.json first. I'll run `npm init` for you (:"
+  );
+  initializeNpm();
+} else if (mustInstallDeps()) {
+  console.log(
+    "Now we just need to install the dependencies required for gulp-tasks to run (:"
+  );
+  installGulpTaskDependencies().then(() =>
+    console.log("You're good to go with `gulp-tasks`. Try running `npm run gulp build`")
+  );
+} else {
+  bootstrapGulp();
 }
 
-try {
-  var requireDir = require("require-dir");
-  requireDir(gulpTasksFolder);
-  if (process.env.TEST_GULP_TASKS) {
-    console.warn("including tasks from tests folder");
-    requireDir(path.join(gulpTasksFolder, "tests"));
-  }
-/* WIP: testing task auto-hooks
-const 
-  generateTaskHooks = requireModule("generate-task-hooks"),
-  gulp = requireModule("gulp-with-help");
-  Object.keys(gulp.tasks).forEach(generateTaskHooks); 
-*/
-  ["override-tasks", "local-tasks"].forEach(function (dirname) {
-    if (fs.existsSync(dirname)) {
-      requireDir(dirname);
-    }
-  });
-} catch (e) {
-  if (shouldDump(e)) {
-    console.error(e);
-  } else {
-    if (!process.env.DEBUG) {
-      console.log([
-        "Error occurred.",
-        "For more info, set the DEBUG environment variable (eg set DEBUG=*).",
-        "If you still get nothing useful, set ALWAYS_DUMP_GULP_ERRORS and report the missed error as an issue"
-      ].join("\n"));
-    }
-  }
-  process.exit(1);
+function requiredDeps() {
+    var starter = require([".", gulpTasksFolder, "start", "package.json"].join("/"));
+    return Object.keys(starter.devDependencies);
 }
 
-function shouldDump(e) {
-  return process.env.ALWAYS_DUMP_GULP_ERRORS || probablyNotReportedByGulp(e);
-}
-
-function probablyNotReportedByGulp(e) {
-  var message = (e || "").toString().toLowerCase();
-  return [
-    /cannot find module/i,
-    /referenceerror/i,
-    /syntaxerror/i,
-    /^Error:/i
-  ].reduce((acc, cur) => {
-    return acc || message.match(cur)
+function mustInstallDeps() {
+  var
+    package = require("./package.json"),
+    devDeps = package.devDependencies || {},
+    haveDeps = Object.keys(devDeps),
+    needDeps = requiredDeps();
+  return needDeps.reduce((acc, cur) => {
+    return acc || haveDeps.indexOf(cur) == -1;
   }, false);
+}
+
+function initializeNpm() {
+  var spawn = requireModule("spawn");
+  runNpmWith(["init"])
+  .then(() => spawn("cmd", [ "/c", "node", process.argv[1]]));
+}
+
+function installGulpTaskDependencies() {
+  var
+    findFirstMissing = function() {
+      var args = Array.from(arguments);
+      return args.reduce((acc, cur) => acc || (fs.existsSync(cur) ? acc : cur), undefined);
+    },
+    deps = requiredDeps(),
+    package = require("./package.json"),
+    buildTools = findFirstMissing("tools", "build-tools", ".tools", ".build-tools"),
+    prepend = `cross-env BUILD_TOOLS_FOLDER=${buildTools}`;
+
+  package.scripts["gulp"] = `${prepend} gulp`;
+  package.scripts["test"] = `run-s "gulp test-dotnet"`;
+  package.scripts["build"] = `run-s "gulp build"`;
+  package.scripts["pack"] = `BUILD_CONFIGURATION=Release run-s "gulp pack"`;
+
+  fs.writeFileSync("package.json", JSON.stringify(package, null, 4), { encoding: "utf8" });
+
+  return runNpmWith(["install", "--save-dev"].concat(deps));
+}
+
+function bootstrapGulp() {
+  var importNpmTasks = requireModule("import-npm-tasks");
+  try {
+    importNpmTasks();
+    var requireDir = require("require-dir");
+    requireDir("gulp-tasks");
+    ["override-tasks", "local-tasks"].forEach(function(dirname) {
+      if (fs.existsSync(dirname)) {
+        requireDir(dirname);
+      }
+    });
+  } catch (e) {
+    if (looksLikeMissingGulpTasksModule(e)) {
+      return installModuleFor(e).then(() => restart());
+    }
+    if (shouldDump(e)) {
+      console.error(e);
+    } else {
+      if (!process.env.DEBUG) {
+        console.log(
+          "Error occurred. For more info, set the DEBUG environment variable (eg set DEBUG=*)."
+        );
+      }
+    }
+    process.exit(1);
+  }
+
+  function shouldDump(e) {
+    return process.env.ALWAYS_DUMP_GULP_ERRORS || probablyNotReportedByGulp(e);
+  }
+
+  function probablyNotReportedByGulp(e) {
+    var message = (e || "").toString().toLowerCase();
+    return ["cannot find module", "referenceerror", "syntaxerror"].reduce(
+      (acc, cur) => {
+        return acc || message.indexOf(cur) > -1;
+      },
+      false
+    );
+  }
+}
+
+function looksLikeMissingGulpTasksModule(e) {
+  if (!e || !e.message) {
+    return false;
+  }
+  var mod = grokMissingPackageFrom(e.message);
+  if (!mod) {
+    return false;
+  }
+  var starter = require("./gulp-tasks/start/package.json");
+  return !!starter.devDependencies[mod];
+}
+
+function installModuleFor(e) {
+  var mod = grokMissingPackageFrom(e.message);
+  console.log(`\n\nLooks like you're missing a package required by gulp-tasks: ${mod}\nLet's get that installed and try again (:`);
+  return runNpmWith(["install", mod]);
+}
+
+function grokMissingPackageFrom(message) {
+  var match = message.match("Cannot find module '(.*)'");
+  return match && match.length > 1 ? match[1] : null;
+}
+
+function restart() {
+  return spawn(process.argv[0], process.argv.slice(1), { env: process.env });
+}
+
+function runNpmWith(args) {
+  var spawn = requireModule("spawn");
+  return os.platform() === "win32"
+    ? spawn("cmd", ["/c", "npm"].concat(args))
+    : spawn("npm", args);
 }
