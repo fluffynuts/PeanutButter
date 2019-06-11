69de4ddfcd465fab3f7e43506ddac6204c86e6bd
Davyd McColl <davydm@gmail.com>
2019-06-11 10:07:48 +0200

    :bug: PeanutButter.INI should multi-target net452 & netstandard2.0

--- source/INI/PeanutButter.INI/Properties/AssemblyInfo.cs
+++ source/INI/PeanutButter.INI/Properties/AssemblyInfo.cs
@@ -4,11 +4,6 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PeanutButter.INI")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PeanutButter.INI")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,16 +15,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("91d4ee7b-82ad-45a7-980b-dc15617cd763")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
