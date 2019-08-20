6b4ad68d934e4bd8eb59d44c2ff3e80f5061173b
Davyd McColl <davydm@gmail.com>
2019-08-20 14:56:51 +0200

    :alembic: better runner scripts, probably

--- source/TempDb/PeanutButter.TempDb.Runner/start-tempdb.bat
+++ source/TempDb/PeanutButter.TempDb.Runner/start-tempdb.bat
@@ -1,14 +1,20 @@
 @echo off
 
-where /Q dotnet
-if %ERRORLEVEL% NEQ 0 goto :no_dotnet
+if exist PeanutButter.TempDb.Runner.exe (
+    @rem running from net framework output
+    PeanutButter.TempDb.Runner.exe %*
+) else (
+    @rem running from netcore output
+    where /Q dotnet
+    if %ERRORLEVEL% NEQ 0 goto :no_dotnet
 
-dotnet %~dp0\PeanutButter.TempDb.Runner.dll %*
+    dotnet %~dp0\PeanutButter.TempDb.Runner.dll %*
 
-goto end
+    goto end
 
-:no_dotnet
-echo dotnet must be found in your path to use this utility
-exit /b 127
+    :no_dotnet
+    echo dotnet must be found in your path to use this utility
+    exit /b 127
 
-:end
+    :end
+)
