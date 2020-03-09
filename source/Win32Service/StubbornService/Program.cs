317991f333d7f0ecbc1f1f9079b0a37c39831c5b
Davyd McColl <davydm@gmail.com>
2020-03-09 15:17:12 +0200

    :wrench: facilitate configuring stubborn-service to deadlock _forever_ (for testing, of course!)

--- source/Win32Service/StubbornService/Program.cs
+++ source/Win32Service/StubbornService/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Configuration;
 using System.ServiceProcess;
 using System.Threading;
 
@@ -19,7 +20,22 @@ namespace StubbornService
             var semaphore = new SemaphoreSlim(1);
             semaphore.Wait();
             // deadlock, on purpose
-            semaphore.Wait(TimeSpan.FromMinutes(1));
+            var howLongToWait = WaitTime;
+            if (howLongToWait < 0)
+            {
+                // deadlock FOREVER
+                semaphore.Wait();
+            }
+            else
+            {
+                // deadlock just for a while (:
+                semaphore.Wait(TimeSpan.FromSeconds(howLongToWait));
+            }
         }
+
+        private static int WaitTime
+            => int.TryParse(ConfigurationManager.AppSettings["WaitTime"], out var result)
+                ? result
+                : 60;
     }
 }
