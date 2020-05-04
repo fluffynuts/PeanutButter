7b7bdbb35cd686a9f70505614e33750f734f9df1
Davyd McColl <davydm@gmail.com>
2020-05-04 14:43:49 +0200

    :art: better shell/bat scripts (might actually work!)

--- source/TempDb/PeanutButter.TempDb.Runner/start-tempdb.bat
+++ source/TempDb/PeanutButter.TempDb.Runner/start-tempdb.bat
@@ -1,20 +1,15 @@
 @echo off
 
-if exist PeanutButter.TempDb.Runner.exe (
+if exist "PeanutButter.TempDb.Runner.exe" (
     @rem running from net framework output
     PeanutButter.TempDb.Runner.exe %*
 ) else (
     @rem running from netcore output
     where /Q dotnet
-    if %ERRORLEVEL% NEQ 0 goto :no_dotnet
-
-    dotnet %~dp0\PeanutButter.TempDb.Runner.dll %*
-
-    goto end
-
-    :no_dotnet
-    echo dotnet must be found in your path to use this utility
-    exit /b 127
-
-    :end
+    if ERRORLEVEL 0 (
+       dotnet %~dp0\PeanutButter.TempDb.Runner.dll %*
+    ) else (
+       echo dotnet must be found in your path to use this utility
+       exit /b 127
+    )
 )
--- source/TempDb/PeanutButter.TempDb.Runner/start-tempdb.sh
+++ source/TempDb/PeanutButter.TempDb.Runner/start-tempdb.sh
@@ -2,19 +2,19 @@
 RUNNER_EXE=$(dirname $0)/PeanutButter.TempDb.Runner.exe
 # "command not found" exit code, according to http://www.tldp.org/LDP/abs/html/exitcodes.html
 ENOENT=127
-if test -e $RUNNER_EXE; then
+if test -e "$RUNNER_EXE"; then
     # running from net framework output
     if test -z "$(which mono)"; then
         echo "mono is required in your path to use tempdb"
         exit #ENOENT;
     fi
-    mono $RUNNER_EXE $@
+    mono "$RUNNER_EXE" "$@"
 else
     # running from netcore output
     if test -z "$(which dotnet)"; then
         echo "dotnet core is required in your path to use tempdb";
         # "command not found" exit code, according to http://www.tldp.org/LDP/abs/html/exitcodes.html
-        exit $ENOENT;
+        exit ${ENOENT};
     fi
-    dotnet $(dirname $0)/PeanutButter.TempDb.Runner.dll $@
+    dotnet "$(dirname $0)/PeanutButter.TempDb.Runner.dll" "$@"
 fi
