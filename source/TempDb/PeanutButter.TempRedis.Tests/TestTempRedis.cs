4396519b03648c85cfe22a15fa7b062870afbf04
Davyd McColl <davydm@gmail.com>
2021-01-28 15:18:06 +0200

    :sparkles: prove that auto-download of redis works

--- source/TempDb/PeanutButter.TempRedis.Tests/TestTempRedis.cs
+++ source/TempDb/PeanutButter.TempRedis.Tests/TestTempRedis.cs
@@ -12,14 +12,42 @@ namespace PeanutButter.TempRedis.Tests
         [Test]
         public void ShouldStartUp()
         {
-            using var tempRedis = new TempRedis();
+            // Arrange
+            using var sut = new TempRedis();
+            
+            // Act
             var connection = ConnectionMultiplexer.Connect(
-                $"localhost:{tempRedis.Port}"
+                $"localhost:{sut.Port}"
             );
             connection.GetDatabase();
             var server = connection.GetEndPoints()
                 .Select(e => connection.GetServer(e))
                 .FirstOrDefault();
+            
+            // Assert
+            Expect(server)
+                .Not.To.Be.Null(() => "Can't determine the redis server");
+        }
+
+        [Test]
+        [Explicit("Requires downloading redis from GitHub")]
+        public void ShouldBeAbleToStartFromAutoDownload()
+        {
+            // Arrange
+            using var sut = new TempRedis(
+                new TempRedisOptions()
+                {
+                    LocatorStrategies = RedisLocatorStrategies.DownloadForWindowsIfNecessary
+                });
+            // Act
+            var connection = ConnectionMultiplexer.Connect(
+                $"localhost:{sut.Port}"
+            );
+            connection.GetDatabase();
+            var server = connection.GetEndPoints()
+                .Select(e => connection.GetServer(e))
+                .FirstOrDefault();
+            // Assert
             Expect(server)
                 .Not.To.Be.Null(() => "Can't determine the redis server");
         }
--- source/TempDb/PeanutButter.TempRedis/TempRedis.cs
+++ source/TempDb/PeanutButter.TempRedis/TempRedis.cs
@@ -6,6 +6,31 @@ using StackExchange.Redis;
 namespace PeanutButter.TempRedis
 {
     /// <summary>
+    /// Strategies for resolving a temporary redis server executable
+    /// </summary>
+    [Flags]
+    public enum RedisLocatorStrategies
+    {
+        /// <summary>
+        /// Look for redis-server.exe or redis-server (linux/osx) in the path
+        /// </summary>
+        FindInPath = 1,
+
+        /// <summary>
+        /// Look for an installed Redis service on windows (does not apply to linux/osx)
+        /// The service must be called 'redis'
+        /// </summary>
+        FindAsWindowsService = 2,
+
+        /// <summary>
+        /// Attempt to download win32 binaries from Microsoft archives at github if
+        /// no redis-server found in the path and the service is not installed or not
+        /// found
+        /// </summary>
+        DownloadForWindowsIfNecessary = 4
+    }
+
+    /// <summary>
     /// Provides a temporary, disposable Redis instance
     /// levering off of a locally installed redis-server
     /// installation or binary within the path
@@ -16,32 +41,63 @@ namespace PeanutButter.TempRedis
         /// The port this instance is listening on
         /// </summary>
         int Port { get; }
-        
+
         /// <summary>
         /// The resolved path to redis-server
         /// (may be passed in as a constructor parameter if necessary)
         /// </summary>
         string RedisExecutable { get; }
-        
+
+        /// <summary>
+        /// The currently-used redis service locator strategies
+        /// </summary>
+        RedisLocatorStrategies LocatorStrategies { get; set; }
+
         /// <summary>
         /// Provides a simple mechanism to open a connection to the
         /// redis instance
         /// </summary>
         /// <returns></returns>
         ConnectionMultiplexer Connect();
-        
+
         /// <summary>
         /// Starts up the server, if not auto-started at construction time
         /// (default is to auto-start)
         /// </summary>
         void Start();
-        
+
         /// <summary>
         /// Stops the redis server. You probably don't need to
         /// call this as the server will automatically be stopped
         /// when disposed
         /// </summary>
         void Stop();
+    }
+
+    /// <summary>
+    /// Options for construction of a TempRedis instance
+    /// </summary>
+    public class TempRedisOptions
+    {
+        /// <summary>
+        /// Auto-start the service on construction? (default true)
+        /// </summary>
+        public bool AutoStart { get; set; } = true;
+
+        /// <summary>
+        /// Provide a custom path to the redis-server executable
+        /// (default not set)
+        /// </summary>
+        public string PathToRedisService { get; set; }
+
+        /// <summary>
+        /// Strategies which may be employed to locate redis
+        /// This is a flag set (default: all strategies)
+        /// </summary>
+        public RedisLocatorStrategies LocatorStrategies { get; set; } =
+            RedisLocatorStrategies.FindInPath |
+            RedisLocatorStrategies.FindAsWindowsService |
+            RedisLocatorStrategies.DownloadForWindowsIfNecessary;
     }
 
     /// <inheritdoc />
@@ -55,8 +111,10 @@ namespace PeanutButter.TempRedis
         /// <inheritdoc />
         public string RedisExecutable =>
             _executable ??= FindRedisExecutable();
+
         private string _executable;
 
+        public RedisLocatorStrategies LocatorStrategies { get; set; }
 
         /// <inheritdoc />
         public ConnectionMultiplexer Connect() =>
@@ -69,32 +127,23 @@ namespace PeanutButter.TempRedis
         /// Constructs an instance of the temporary server,
         /// automatically started up and using redis-server
         /// as found in your PATH or via Windows service registry,
-        /// where applicable.
+        /// where applicable; will attempt to download redis server
+        /// on a windows host if not found in your path or as a service
         /// </summary>
-        public TempRedis() : this(true)
-        {
-        }
-
-        /// <summary>
-        /// Construct an instance of the temporary server with the
-        /// default mechanisms for finding the redis-server executable
-        /// and only auto-starts if autoStart is true
-        /// </summary>
-        /// <param name="autoStart">flag: whether the server should be automatically started</param>
-        public TempRedis(bool autoStart) : this(autoStart, null)
+        public TempRedis() : this(new TempRedisOptions())
         {
         }
 
         /// <summary>
         /// Construct an instance of the temporary server
         /// </summary>
-        /// <param name="autoStart">flag: whether the server should be automatically started</param>
-        /// <param name="pathToRedisServer">override path to redis-server executable</param>
-        public TempRedis(bool autoStart, string pathToRedisServer)
+        /// <param name="options"></param>
+        public TempRedis(TempRedisOptions options)
         {
-            _executable = pathToRedisServer;
+            _executable = options.PathToRedisService;
+            LocatorStrategies = options.LocatorStrategies;
             Port = FindOpenRandomPort();
-            if (autoStart)
+            if (options.AutoStart)
             {
                 Start();
             }
@@ -159,28 +208,46 @@ namespace PeanutButter.TempRedis
 
         private string FindRedisExecutable()
         {
-            // prefer redis-server in the path
-            var inPath = Find.InPath("redis-server");
-            if (inPath is not null)
+            var lookInPath = LocatorStrategies.HasFlag(RedisLocatorStrategies.FindInPath);
+            if (lookInPath)
             {
-                return inPath;
+                // prefer redis-server in the path
+                var inPath = Find.InPath("redis-server");
+                if (inPath is not null)
+                {
+                    return inPath;
+                }
             }
 
             if (Platform.IsUnixy)
             {
                 throw new NotSupportedException(
-                    $"{nameof(TempRedis)} requires redis-server to be in your path. Is redis installed on this system?"
+                    lookInPath
+                        ? $"{nameof(TempRedis)} requires redis-server to be in your path for this platform. Is redis installed on this system?"
+                        : $"{nameof(TempRedis)} only supports finding redis-server in your path for this platform. Please enable the flag or pass in a path to redis-server"
                 );
             }
 
-            var serviceExePath = RedisWindowsServiceFinder.FindPathToRedis();
-            if (serviceExePath is not null)
+            var lookForService = LocatorStrategies.HasFlag(RedisLocatorStrategies.FindAsWindowsService);
+            if (lookForService)
             {
-                return serviceExePath;
+                var serviceExePath = RedisWindowsServiceFinder.FindPathToRedis();
+                if (serviceExePath is not null)
+                {
+                    return serviceExePath;
+                }
+            }
+
+            var attemptDownload = LocatorStrategies.HasFlag(RedisLocatorStrategies.DownloadForWindowsIfNecessary);
+            if (!attemptDownload)
+            {
+                throw new NotSupportedException(
+                    GenerateFailMessageFor(lookInPath, lookForService, false, null)
+                );
             }
 
             var downloadError = "unknown";
-            var downloadedPath = Async.RunSync(() =>
+            var result = Async.RunSync(() =>
             {
                 var downloader = new MicrosoftRedisDownloader();
                 try
@@ -193,10 +260,38 @@ namespace PeanutButter.TempRedis
                     return null;
                 }
             });
-                
+            
+            if (result is not null)
+            {
+                return result;
+            }
+
             throw new NotSupportedException(
-                $"{nameof(TempRedis)} requires either redis-server.exe in your path, or the Redis windows service to be installed. Additionally, an attempt to download redis from github resulted in the following error: {downloadError}"
+                GenerateFailMessageFor(lookInPath, lookForService, true, downloadError)
             );
+
+        }
+
+        private static string GenerateFailMessageFor(
+            bool lookInPath,
+            bool lookForService,
+            bool attemptDownload,
+            string downloadError
+        )
+        {
+            return new[]
+            {
+                "Unable to start up a temporary redis instance: no redis-server.exe could be found",
+                lookInPath
+                    ? "* Try adding the folder containing redis-server.exe to your path"
+                    : "* Try enabling the FindInPath location strategy",
+                lookForService
+                    ? "* Try installing the Redis windows service (must be called 'redis' to be found)"
+                    : "* Try enabling a search for a locally-installed Redis service",
+                attemptDownload
+                    ? $"* Unable to download Redis from github/Microsoft/archive: {downloadError}"
+                    : "* Try enabling auto-download of Redis from github/Microsoft/archive"
+            }.JoinWith("\n");
         }
     }
 }
