09c84033cdd09c769fd047987caceb55947ea306
Davyd McColl <davydm@gmail.com>
2021-06-14 17:05:55 +0200

    :sparkles: expose all FileStream options

--- source/Utils/PeanutButter.Utils/AutoTempFolder.cs
+++ source/Utils/PeanutButter.Utils/AutoTempFolder.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+// ReSharper disable MemberCanBePrivate.Global
 
 namespace PeanutButter.Utils
 {
@@ -12,14 +13,12 @@ namespace PeanutButter.Utils
     public interface IAutoTempFolder : IDisposable
     {
         /// <summary>
-        /// Returns the path to the created folder
+        /// The local path at which this temp folder exists
         /// </summary>
         string Path { get; }
 
         /// <summary>
-        /// Provides a full path into the temp folder for the provided path parts
-        /// - if the provided path resolves inside the temp folder already, it will
-        ///   be returned as-is
+        /// Resolves a relative path (or path parts) within the auto temp folder
         /// </summary>
         /// <param name="p1"></param>
         /// <param name="more"></param>
@@ -30,69 +29,129 @@ namespace PeanutButter.Utils
         );
 
         /// <summary>
-        /// Creates the desired relative-pathed directory, if it doesn't
-        /// exist already
+        /// Creates a directory within the auto temp folder
+        /// - any supporting directories will also be created
         /// </summary>
         /// <param name="dirname"></param>
-        /// <exception cref="NotImplementedException"></exception>
+        /// <returns></returns>
         string CreateDirectory(string dirname);
 
         /// <summary>
-        /// Writes text to the provided relative file path
+        /// Writes a file within the auto temp folder
         /// </summary>
-        /// <param name="filename"></param>
-        /// <param name="data"></param>
-        /// <returns>full path to the file</returns>
-        /// <exception cref="NotImplementedException"></exception>
+        /// <param name="filename">relative path to the file</param>
+        /// <param name="data">string data to write</param>
+        /// <returns>full path to the written file</returns>
         string WriteFile(
             string filename,
             string data
         );
 
         /// <summary>
-        /// Writes data to the provided relative file path
+        /// Writes a file within the auto temp folder
         /// </summary>
-        /// <param name="filename"></param>
-        /// <param name="data"></param>
-        /// <returns>full path to the file</returns>
+        /// <param name="filename">relative path to the file</param>
+        /// <param name="data">binary data to write</param>
+        /// <returns>full path to the written file</returns>
         string WriteFile(
             string filename,
             byte[] data
         );
 
         /// <summary>
-        /// Writes data to the provided relative file path
+        /// Writes a file within the auto temp folder
         /// </summary>
-        /// <param name="filename"></param>
-        /// <param name="data"></param>
-        /// <returns>full path to the file</returns>
+        /// <param name="filename">relative path to the file</param>
+        /// <param name="data">stream data to write</param>
+        /// <returns>full path to the written file</returns>
         string WriteFile(
             string filename,
             Stream data
         );
 
         /// <summary>
-        /// Reads a text file
+        /// Reads a text file by relative path within the auto temp folder
         /// </summary>
         /// <param name="filename"></param>
         /// <returns></returns>
         string ReadTextFile(string filename);
 
         /// <summary>
-        /// Reads a binary file
+        /// Reads a binary file by relative path within the auto temp folder
         /// </summary>
         /// <param name="filename"></param>
         /// <returns></returns>
         byte[] ReadFile(string filename);
 
         /// <summary>
-        /// Opens the relative path as a file with the desired access
-        /// - will dispose the handle when this auto-temp-folder is disposed
+        /// Opens a file (read-only) by relative path within the auto temp folder
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        FileStream OpenFile(string filename);
+
+        /// <summary>
+        /// Opens a file by relative path within the auto temp folder
+        /// with the desired access
+        /// - defaulted share is None
+        /// - defaulted buffer size is 4096
+        /// - defaulted options is None
         /// </summary>
         /// <param name="filename"></param>
         /// <param name="access"></param>
         /// <returns></returns>
-        FileStream OpenFile(string filename, FileAccess access);
+        FileStream OpenFile(
+            string filename,
+            FileAccess access
+        );
+
+        /// <summary>
+        /// Opens a file by relative path within the auto temp folder
+        /// with the desired access and share
+        /// - defaulted buffer size is 4096
+        /// - defaulted options is None
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <param name="access"></param>
+        /// <param name="share"></param>
+        FileStream OpenFile(
+            string filename,
+            FileAccess access,
+            FileShare share
+        );
+
+        /// <summary>
+        /// Opens a file by relative path within the auto temp folder
+        /// with the desired access, share and buffer size
+        /// - defaulted options is None
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <param name="access"></param>
+        /// <param name="share"></param>
+        /// <param name="bufferSize"></param>
+        FileStream OpenFile(
+            string filename,
+            FileAccess access,
+            FileShare share,
+            int bufferSize
+        );
+
+        /// <summary>
+        /// Opens a file by relative path within the auto temp folder
+        /// with the desired access, share, buffer size and options
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <param name="access"></param>
+        /// <param name="share"></param>
+        /// <param name="bufferSize"></param>
+        /// <param name="options"></param>
+        FileStream OpenFile(
+            string filename,
+            FileAccess access,
+            FileShare share,
+            int bufferSize,
+            FileOptions options
+        );
     }
 
     /// <summary>
@@ -101,6 +160,26 @@ namespace PeanutButter.Utils
     /// </summary>
     public class AutoTempFolder : IAutoTempFolder
     {
+        /// <summary>
+        /// Default file access, as per System.IO.FileStream
+        /// </summary>
+        public const FileAccess DEFAULT_FILE_ACCESS = FileAccess.Read;
+
+        /// <summary>
+        /// Default file share mode as per System.IO.FileStream
+        /// </summary>
+        public const FileShare DEFAULT_FILE_SHARE = FileShare.None;
+
+        /// <summary>
+        /// Default buffer size as per System.IO.FileStream
+        /// </summary>
+        public const int DEFAULT_BUFFER_SIZE = 4096; // this is the default in FileStream.cs
+
+        /// <summary>
+        /// Default file options, as per System.IO.FileStream
+        /// </summary>
+        public const FileOptions DEFAULT_FILE_OPTIONS = FileOptions.None;
+
         /// <inheritdoc />
         public string Path { get; private set; }
 
@@ -260,10 +339,74 @@ namespace PeanutButter.Utils
         }
 
         /// <inheritdoc />
+        public FileStream OpenFile(string filename)
+        {
+            return OpenFile(filename, DEFAULT_FILE_ACCESS);
+        }
+
+        /// <inheritdoc />
         public FileStream OpenFile(string filename, FileAccess access)
         {
+            return OpenFile(
+                filename,
+                access,
+                DEFAULT_FILE_SHARE
+            );
+        }
+
+
+        /// <inheritdoc />
+        public FileStream OpenFile(
+            string filename,
+            FileAccess access,
+            FileShare share
+        )
+        {
+            return OpenFile(
+                filename,
+                access,
+                share,
+                DEFAULT_BUFFER_SIZE
+            );
+        }
+
+        /// <inheritdoc />
+        public FileStream OpenFile(
+            string filename,
+            FileAccess access,
+            FileShare share,
+            int bufferSize
+        )
+        {
+            return OpenFile(
+                filename,
+                access,
+                share,
+                bufferSize,
+                DEFAULT_FILE_OPTIONS
+            );
+        }
+
+        /// <inheritdoc />
+        public FileStream OpenFile(
+            string filename,
+            FileAccess access,
+            FileShare share,
+            int bufferSize,
+            FileOptions options
+        )
+        {
             var fullPath = ResolvePath(filename);
-            return Store(new FileStream(fullPath, FileMode.OpenOrCreate, access));
+            return Store(
+                new FileStream(
+                    fullPath,
+                    FileMode.OpenOrCreate,
+                    access,
+                    share,
+                    bufferSize,
+                    options
+                )
+            );
         }
 
         private readonly List<FileStream> _fileStreams = new List<FileStream>();
