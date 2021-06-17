670be935072b54fb574bc3108d70118f6f2d065b
Davyd McColl <davydm@gmail.com>
2021-06-17 09:03:41 +0200

    :truck: move enumerator near only consumer, like other dict enumerators

--- source/Utils/PeanutButter.Utils/Dictionaries/DictionaryWrappingObject.cs
+++ source/Utils/PeanutButter.Utils/Dictionaries/DictionaryWrappingObject.cs
@@ -645,4 +645,39 @@ namespace PeanutButter.Utils.Dictionaries
             _valueSetter = valueSetter;
         }
     }
+    
+    internal class DictionaryWrappingObjectEnumerator : IEnumerator<KeyValuePair<string, object>>
+    {
+        private readonly DictionaryWrappingObject _dict;
+        private int _index;
+        private readonly string[] _keys;
+
+        public DictionaryWrappingObjectEnumerator(DictionaryWrappingObject dict)
+        {
+            _dict = dict;
+            _keys = _dict.Keys.ToArray();
+            Reset();
+        }
+
+        public void Dispose()
+        {
+            /* intentionally left blank */
+        }
+
+        public bool MoveNext()
+        {
+            return ++_index < _keys.Length;
+        }
+
+        public void Reset()
+        {
+            _index = -1;
+        }
+
+        public KeyValuePair<string, object> Current =>
+            new KeyValuePair<string, object>(_keys[_index], _dict[_keys[_index]]);
+
+        object IEnumerator.Current => Current;
+    }
+    
 }
