f68d63439dba6a7b4a226c86950c1879fd2e0751
Davyd McColl <davydm@gmail.com>
2022-04-07 10:51:33 +0200

    :bug: fix invalid namespacing

--- source/Utils/PeanutButter.Utils/AutoResetter.cs
+++ source/Utils/PeanutButter.Utils/AutoResetter.cs
@@ -2,52 +2,53 @@
 
 namespace
 #if BUILD_PEANUTBUTTER_INTERNAL
-    Imported.PeanutButter.Utils;
+    Imported.PeanutButter.Utils
 #else
-PeanutButter.Utils
+    PeanutButter.Utils
 #endif
-
-/// <summary>
-/// Provides a mechanism to run code on construction and disposal,
-/// irrespective of exception handling
-/// Use this, for example, to set up and tear down state required for
-/// a test -- your constructionAction is called immediately upon construction
-/// and the using() pattern guarantees that your disposalAction is called at
-/// disposal, even if your test fails.
-/// </summary>
+{
+    /// <summary>
+    /// Provides a mechanism to run code on construction and disposal,
+    /// irrespective of exception handling
+    /// Use this, for example, to set up and tear down state required for
+    /// a test -- your constructionAction is called immediately upon construction
+    /// and the using() pattern guarantees that your disposalAction is called at
+    /// disposal, even if your test fails.
+    /// </summary>
 #if BUILD_PEANUTBUTTER_INTERNAL
 internal
 #else
-public
+    public
 #endif
-    class AutoResetter : IDisposable
-{
-    private readonly object _lock = new object();
-    private Action _disposalAction;
-
-    /// <summary>
-    /// Constructs a new AutoResetter and immediately runs the constructionAction
-    /// </summary>
-    /// <param name="constructionAction">Action to run at construction time</param>
-    /// <param name="disposalAction">Action to run at disposal time</param>
-    public AutoResetter(Action constructionAction, Action disposalAction)
+        class AutoResetter : IDisposable
     {
-        constructionAction();
-        _disposalAction = disposalAction;
-    }
+        private readonly object _lock = new object();
+        private Action _disposalAction;
 
-    /// <inheritdoc />
-    public void Dispose()
-    {
-        lock (_lock)
+        /// <summary>
+        /// Constructs a new AutoResetter and immediately runs the constructionAction
+        /// </summary>
+        /// <param name="constructionAction">Action to run at construction time</param>
+        /// <param name="disposalAction">Action to run at disposal time</param>
+        public AutoResetter(Action constructionAction, Action disposalAction)
         {
-            try
+            constructionAction();
+            _disposalAction = disposalAction;
+        }
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            lock (_lock)
             {
-                _disposalAction?.Invoke();
-            }
-            finally
-            {
-                _disposalAction = null;
+                try
+                {
+                    _disposalAction?.Invoke();
+                }
+                finally
+                {
+                    _disposalAction = null;
+                }
             }
         }
     }
