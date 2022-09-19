2fd6a21cdf4d3191931f6d03ce2374f894b8bb85
Davyd McColl <davydm@gmail.com>
2022-09-19 10:27:28 +0200

    :sparkles: add rudimentary ViewDataDictionaryBuilder

---
+++ source/TestUtils/PeanutButter.TestUtils.AspNetCore.Tests/TestViewDataDictionaryBuilder.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using NExpect;
+using PeanutButter.TestUtils.AspNetCore.Builders;
+using static NExpect.Expectations;
+using static PeanutButter.RandomGenerators.RandomValueGen;
+
+namespace PeanutButter.TestUtils.AspNetCore.Tests
+{
+    [TestFixture]
+    public class TestViewDataDictionaryBuilder
+    {
+        [TestFixture]
+        public class BuildDefault
+        {
+            [Test]
+            public void ShouldBuildAValidViewData()
+            {
+                // Arrange
+                // Act
+                var result = ViewDataDictionaryBuilder.BuildDefault();
+                // Assert
+                Expect(result)
+                    .Not.To.Be.Null();
+                Expect(result.Model)
+                    .Not.To.Be.Null();
+            }
+        }
+
+        [Test]
+        public void ShouldBeAbleToSetModel()
+        {
+            // Arrange
+            var model = new { Id = 1 };
+            // Act
+            var result = ViewDataDictionaryBuilder.Create()
+                .WithModel(model)
+                .Build();
+            // Assert
+            Expect(result.Model)
+                .To.Be(model);
+        }
+    }
+}
---
+++ source/TestUtils/PeanutButter.TestUtils.AspNetCore/Builders/ViewDataDictionaryBuilder.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Internal;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.Extensions.Options;
+
+namespace PeanutButter.TestUtils.AspNetCore.Builders
+{
+    /// <summary>
+    /// Build a ViewDataDictionary
+    /// </summary>
+    public class ViewDataDictionaryBuilder
+        : Builder<ViewDataDictionaryBuilder, ViewDataDictionary>
+    {
+        /// <inheritdoc />
+        public ViewDataDictionaryBuilder()
+        {
+            WithModel(new object());
+        }
+
+        /// <summary>
+        /// Set the model on the ViewDataDictionary
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public ViewDataDictionaryBuilder WithModel(object model)
+        {
+            return With(o => o.Model = model);
+        }
+
+        /// <inheritdoc />
+        protected override ViewDataDictionary ConstructEntity()
+        {
+            var compositeProvider = new DefaultCompositeMetadataDetailsProvider(
+                new[] { new DefaultBindingMetadataProvider() }
+            );
+            var optionsAccessor = new OptionsAccessor();
+            var provider = new DefaultModelMetadataProvider(
+                compositeProvider,
+                optionsAccessor
+            );
+            return new ViewDataDictionary(
+                provider,
+                new ModelStateDictionary()
+            );
+
+        }
+        
+        /// <summary>
+        /// Implementation of IOptions&lt;MvcOptions&gt; to be used
+        /// for generating ViewDataDictionaries, or whatever else
+        /// you might like (:
+        /// </summary>
+        public class OptionsAccessor : IOptions<MvcOptions>
+        {
+            /// <inheritdoc />
+            public MvcOptions Value { get; } = new MvcOptions();
+        }
+
+    }
+}
