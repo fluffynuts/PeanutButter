07a90b6ef92632fc53334c0559037ef257a213c9
Davyd McColl <davydm@gmail.com>
2023-11-14 16:03:15 +0200

    :white_check_mark: abort the test if the caller doesn't have rights to stop the service

--- source/TempDb/PeanutButter.TempDb.Tests/TestMySqlServiceFinder.cs
+++ source/TempDb/PeanutButter.TempDb.Tests/TestMySqlServiceFinder.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 using NExpect;
 using PeanutButter.TempDb.MySql.Base;
@@ -22,8 +23,19 @@ namespace PeanutButter.TempDb.Tests
                 return;
             }
 
-            var svc = new WindowsServiceUtil("mysql80");
-            svc.Stop();
+            try
+            {
+                var svc = new WindowsServiceUtil("mysql80");
+                svc.Stop();
+            }
+            catch (ServiceControlException ex)
+            {
+                if (ex.Message.Contains("access is denied", StringComparison.OrdinalIgnoreCase))
+                {
+                    Assert.Ignore("test requires admin privileges to run");
+                }
+            }
+
             // Act
             var path = MySqlWindowsServiceFinder.FindPathToMySqlD();
             // Assert
