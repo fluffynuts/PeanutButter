a08202cda775bb0b76de9fd5401e04c92049a123
Davyd McColl <davydm@gmail.com>
2024-02-15 16:54:16 +0200

    :sparkles: add .Negate() extension for booleans: modifies in-place and returns the modified value

---
+++ source/Utils/PeanutButter.Utils.NetCore.Tests/TestBooleanExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace PeanutButter.Utils.Tests;
+
+[TestFixture]
+public class TestBooleanExtensions
+{
+    [TestFixture]
+    public class Negate
+    {
+        [Test]
+        public void ShouldNegateTheValueInPlaceAndReturnIt()
+        {
+            // Arrange
+            var value = GetRandomBoolean();
+            var expected = !value;
+            // Act
+            var result = value.Negate();
+            // Assert
+            Expect(result)
+                .To.Equal(expected);
+            Expect(value)
+                .To.Equal(expected);
+        }
+    }
+}
---
+++ source/Utils/PeanutButter.Utils/BooleanExtensions.cs
@@ -0,0 +1,30 @@
+﻿#if BUILD_PEANUTBUTTER_INTERNAL
+namespace Imported.PeanutButter.Utils;
+#else
+namespace PeanutButter.Utils;
+#endif
+/// <summary>
+/// Provides extensions for boolean values
+/// </summary>
+#if BUILD_PEANUTBUTTER_INTERNAL
+    internal
+#else
+public
+#endif
+    static class BooleanExtensions
+{
+    /// <summary>
+    /// Negates the given boolean in-place: if
+    /// you do flag.Negate(), the value held in flag
+    /// should be negated and returned.
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public static bool Negate(
+        this ref bool value
+    )
+    {
+        value = !value;
+        return value;
+    }
+}
