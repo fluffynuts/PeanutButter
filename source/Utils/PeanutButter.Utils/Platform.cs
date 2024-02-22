28486466f5a3b96ca903d38b692e4ccf25dd21b8
Davyd McColl <davydm@gmail.com>
2024-02-22 13:11:13 +0200

    :bug: Runtime should compile internal when BUILD_PEANUTBUTTER_INTERNAL is defined'
    - also move out to own file
    - add IsMacOSX to Platform

--- source/Utils/PeanutButter.Utils/Platform.cs
+++ source/Utils/PeanutButter.Utils/Platform.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Diagnostics.Contracts;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text.RegularExpressions;
 
 #if BUILD_PEANUTBUTTER_INTERNAL
 namespace Imported.PeanutButter.Utils;
@@ -22,7 +19,7 @@ internal
     private static readonly PlatformID[] UnixOperatingSystems =
     {
         PlatformID.Unix,
-        PlatformID.MacOSX
+        PlatformID.MacOSX,
     };
 
     private static readonly PlatformID[] WindowsOperatingSystems =
@@ -51,6 +48,12 @@ internal
         ?? false;
 
     private static bool? _isWindows;
+    
+    /// <summary>
+    /// True when the current platform is MacOSX
+    /// </summary>
+    public static bool IsMacOSX => _isMacOSX ??= Environment.OSVersion.Platform == PlatformID.MacOSX;
+    private static bool? _isMacOSX;
 
     /// <summary>
     /// Are we running 64-bit? Note: you may be in a 32-bit runtime
@@ -70,55 +73,4 @@ internal
     public static string PathDelimiter => IsUnixy
         ? "/"
         : "\\";
-}
-
-/// <summary>
-/// Used to determine what runtime we're currently running on
-/// - usually this is to distinguish between dotnet and mono,
-/// but there could be other use-cases.
-/// </summary>
-public static class Runtime
-{
-    /// <summary>
-    /// Returns true if the runtime is mono
-    /// </summary>
-    public static bool IsMono =>
-        _isMono ??= Type.GetType("Mono.Runtime") is not null;
-
-    private static bool? _isMono;
-
-    /// <summary>
-    /// Returns true if the runtime is .NET Framework
-    /// </summary>
-    public static bool IsNetFx =>
-        _isNetFx ??= RuntimeInformation.FrameworkDescription.StartsWith(
-            ".NET Framework",
-            StringComparison.OrdinalIgnoreCase
-        );
-
-    private static bool? _isNetFx;
-
-    /// <summary>
-    /// Returns true if the runtime is dotnet core
-    /// </summary>
-    public static bool IsNetCore =>
-        _isNetCore ??= NetCoreMatcher.IsMatch(RuntimeInformation.FrameworkDescription);
-
-    private static readonly Regex NetCoreMatcher = new(
-        "^.NET (Core|[\\d.]+)",
-        RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.IgnoreCase
-    );
-
-    private static bool? _isNetCore;
-
-    /// <summary>
-    /// Returns true if the runtime is .NET Native
-    /// </summary>
-    public static bool IsNetNative =>
-        _isNetNative ??= RuntimeInformation.FrameworkDescription.StartsWith(
-            ".NET Native",
-            StringComparison.OrdinalIgnoreCase
-        );
-
-    private static bool? _isNetNative;
 }
---
+++ source/Utils/PeanutButter.Utils/Runtime.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+#if BUILD_PEANUTBUTTER_INTERNAL
+namespace Imported.PeanutButter.Utils;
+#else
+namespace PeanutButter.Utils;
+#endif
+/// <summary>
+/// Used to determine what runtime we're currently running on
+/// - usually this is to distinguish between dotnet and mono,
+/// but there could be other use-cases.
+/// </summary>
+#if BUILD_PEANUTBUTTER_INTERNAL
+internal
+#else
+public
+#endif
+    static class Runtime
+{
+    /// <summary>
+    /// Returns true if the runtime is mono
+    /// </summary>
+    public static bool IsMono =>
+        _isMono ??= Type.GetType("Mono.Runtime") is not null;
+
+    private static bool? _isMono;
+
+    /// <summary>
+    /// Returns true if the runtime is .NET Framework
+    /// </summary>
+    public static bool IsNetFx =>
+        _isNetFx ??= RuntimeInformation.FrameworkDescription.StartsWith(
+            ".NET Framework",
+            StringComparison.OrdinalIgnoreCase
+        );
+
+    private static bool? _isNetFx;
+
+    /// <summary>
+    /// Returns true if the runtime is dotnet core
+    /// </summary>
+    public static bool IsNetCore =>
+        _isNetCore ??= NetCoreMatcher.IsMatch(RuntimeInformation.FrameworkDescription);
+
+    private static readonly Regex NetCoreMatcher = new(
+        "^.NET (Core|[\\d.]+)",
+        RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.IgnoreCase
+    );
+
+    private static bool? _isNetCore;
+
+    /// <summary>
+    /// Returns true if the runtime is .NET Native
+    /// </summary>
+    public static bool IsNetNative =>
+        _isNetNative ??= RuntimeInformation.FrameworkDescription.StartsWith(
+            ".NET Native",
+            StringComparison.OrdinalIgnoreCase
+        );
+
+    private static bool? _isNetNative;
+}
