b9523c2e037fad5ddb5dd6fd3c544a2316ad08fb
Davyd McColl <davydm@gmail.com>
2024-04-04 16:06:28 +0200

    :truck: move HeaderDictionaryExtensions to own file

--- source/TestUtils/PeanutButter.TestUtils.AspNetCore/HttpResponseExtensions.cs
+++ source/TestUtils/PeanutButter.TestUtils.AspNetCore/HttpResponseExtensions.cs
@@ -12,57 +12,6 @@ namespace PeanutButter.TestUtils.AspNetCore;
 #endif
 
 /// <summary>
-/// Provides extensions for the IHeaderDictionary properties
-/// on HttpRequest and HttpResponse
-/// </summary>
-#if BUILD_PEANUTBUTTER_INTERNAL
-internal
-#else
-public
-#endif
-    static class HeaderDictionaryExtensions
-{
-    /// <summary>
-    /// Reads the SameSite attribute for a cookie from the headers,
-    /// since System.Net.Cookie doesn't expose this
-    /// </summary>
-    /// <param name="headers"></param>
-    /// <param name="cookieName"></param>
-    /// <returns></returns>
-    /// <exception cref="CookieNotFoundException"></exception>
-    /// <exception cref="InvalidSameSiteValueException"></exception>
-    public static SameSiteMode ReadSameSiteForCookie(
-        this IHeaderDictionary headers,
-        string cookieName
-    )
-    {
-        var cookieHeader = headers.Where(
-            h => h.Key.Equals("Set-Cookie", StringComparison.OrdinalIgnoreCase)
-        ).Select(
-            header => header.Value.FirstOrDefault(s => s.StartsWith($"{cookieName}="))
-        ).FirstOrDefault();
-        if (cookieHeader is null)
-        {
-            throw new CookieNotFoundException(cookieName);
-        }
-
-        var parts = cookieHeader.Split(";").Trim();
-        foreach (var part in parts)
-        {
-            var subs = part.Split('=').Trim().ToArray();
-            if (subs[0].Equals("SameSite", StringComparison.OrdinalIgnoreCase))
-            {
-                return Enum.TryParse<SameSiteMode>(subs[1], out var parsed)
-                    ? parsed
-                    : throw new InvalidSameSiteValueException(subs[1]);
-            }
-        }
-
-        return SameSiteMode.None;
-    }
-}
-
-/// <summary>
 /// Provides extensions for asp.net HttpResponse objects
 /// </summary>
 public static class HttpResponseExtensions
