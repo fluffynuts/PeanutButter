fcb2f3bc16d8ce21f7fc40865409ccf162cd5ecc
Davyd McColl <davydm@gmail.com>
2024-04-04 15:56:05 +0200

    :sparkles: provide some of the functionality for TimeSpan instances that DateTime instances have
    - AddDays, AddHours, AddSeconds, AddMilliSeconds, AddMilliseconds
    - TruncateHours, TruncateMinutes, TruncateSeconds, TruncateMilliseconds

---
+++ source/Utils/PeanutButter.Utils.NetCore.Tests/TestTimeSpanExtensions.cs
@@ -0,0 +1,193 @@
+﻿namespace PeanutButter.Utils.Tests;
+
+[TestFixture]
+public class TestTimeSpanExtensions
+{
+    [TestFixture]
+    public class Truncation
+    {
+        [Test]
+        public void TruncateMilliseconds()
+        {
+            // Arrange
+            var input = GetRandomTimeSpan();
+            // Act
+            var result = input.TruncateMilliseconds();
+            // Assert
+            Expect(result.Days)
+                .To.Equal(input.Days);
+            Expect(result.Hours)
+                .To.Equal(input.Hours);
+            Expect(result.Minutes)
+                .To.Equal(input.Minutes);
+            Expect(result.Seconds)
+                .To.Equal(input.Seconds);
+            Expect(result.Milliseconds)
+                .To.Equal(0);
+        }
+
+        [Test]
+        public void TruncateSeconds()
+        {
+            // Arrange
+            var input = GetRandomTimeSpan();
+            // Act
+            var result = input.TruncateSeconds();
+            // Assert
+            Expect(result.Days)
+                .To.Equal(input.Days);
+            Expect(result.Hours)
+                .To.Equal(input.Hours);
+            Expect(result.Minutes)
+                .To.Equal(input.Minutes);
+            Expect(result.Seconds)
+                .To.Equal(0);
+            Expect(result.Milliseconds)
+                .To.Equal(0);
+        }
+
+        [Test]
+        public void TruncateMinutes()
+        {
+            // Arrange
+            var input = GetRandomTimeSpan();
+            // Act
+            var result = input.TruncateMinutes();
+            // Assert
+            Expect(result.Days)
+                .To.Equal(input.Days);
+            Expect(result.Hours)
+                .To.Equal(input.Hours);
+            Expect(result.Minutes)
+                .To.Equal(0);
+            Expect(result.Seconds)
+                .To.Equal(0);
+            Expect(result.Milliseconds)
+                .To.Equal(0);
+        }
+
+        [Test]
+        public void TruncateHours()
+        {
+            // Arrange
+            var input = GetRandomTimeSpan();
+            // Act
+            var result = input.TruncateHours();
+            // Assert
+            Expect(result.Days)
+                .To.Equal(input.Days);
+            Expect(result.Hours)
+                .To.Equal(0);
+            Expect(result.Minutes)
+                .To.Equal(0);
+            Expect(result.Seconds)
+                .To.Equal(0);
+            Expect(result.Milliseconds)
+                .To.Equal(0);
+        }
+    }
+
+    [TestFixture]
+    public class DateTimeLikeAddingMethods
+    {
+        [TestFixture]
+        public class AddDays
+        {
+            [Test]
+            public void ShouldAddTheSpecifiedNumberOfDays()
+            {
+                // Arrange
+                var timeSpan = GetRandomTimeSpan();
+                var toAdd = GetRandomDouble(0, 5);
+                var expected = timeSpan.Add(
+                    TimeSpan.FromDays(toAdd)
+                );
+                // Act
+                var result = timeSpan.AddDays(toAdd);
+                // Assert
+                Expect(result)
+                    .To.Equal(expected);
+            }
+        }
+
+        [TestFixture]
+        public class AddHours
+        {
+            [Test]
+            public void ShouldAddTheSpecifiedNumberOfHours()
+            {
+                // Arrange
+                var timeSpan = GetRandomTimeSpan();
+                var toAdd = GetRandomDouble(0, 5);
+                var expected = timeSpan.Add(
+                    TimeSpan.FromHours(toAdd)
+                );
+                // Act
+                var result = timeSpan.AddHours(toAdd);
+                // Assert
+                Expect(result)
+                    .To.Equal(expected);
+            }
+        }
+
+        [TestFixture]
+        public class AddMinutes
+        {
+            [Test]
+            public void ShouldAddTheSpecifiedNumberOfMinutes()
+            {
+                // Arrange
+                var timeSpan = GetRandomTimeSpan();
+                var toAdd = GetRandomDouble(0, 5);
+                var expected = timeSpan.Add(
+                    TimeSpan.FromMinutes(toAdd)
+                );
+                // Act
+                var result = timeSpan.AddMinutes(toAdd);
+                // Assert
+                Expect(result)
+                    .To.Equal(expected);
+            }
+        }
+
+        [TestFixture]
+        public class AddSeconds
+        {
+            [Test]
+            public void ShouldAddTheSpecifiedNumberOfSeconds()
+            {
+                // Arrange
+                var timeSpan = GetRandomTimeSpan();
+                var toAdd = GetRandomDouble(0, 5);
+                var expected = timeSpan.Add(
+                    TimeSpan.FromSeconds(toAdd)
+                );
+                // Act
+                var result = timeSpan.AddSeconds(toAdd);
+                // Assert
+                Expect(result)
+                    .To.Equal(expected);
+            }
+        }
+
+        [TestFixture]
+        public class AddMilliseconds
+        {
+            [Test]
+            public void ShouldAddTheSpecifiedNumberOfMilliseconds()
+            {
+                // Arrange
+                var timeSpan = GetRandomTimeSpan();
+                var toAdd = GetRandomDouble(0, 5);
+                var expected = timeSpan.Add(
+                    TimeSpan.FromMilliseconds(toAdd)
+                );
+                // Act
+                var result = timeSpan.AddMilliseconds(toAdd);
+                // Assert
+                Expect(result)
+                    .To.Equal(expected);
+            }
+        }
+    }
+}
---
+++ source/Utils/PeanutButter.Utils/TimeSpanExtensions.cs
@@ -0,0 +1,182 @@
+﻿using System;
+
+#if BUILD_PEANUTBUTTER_INTERNAL
+namespace Imported.PeanutButter.Utils;
+#else
+namespace PeanutButter.Utils;
+#endif
+/// <summary>
+/// Adds methods similar to those found on
+/// DateTime to TimeSpan
+/// </summary>
+#if BUILD_PEANUTBUTTER_INTERNAL
+internal
+#else
+public
+#endif
+    static class TimeSpanExtensions
+{
+    /// <summary>
+    /// Add this many days to the TimeSpan,
+    /// returning the new value
+    /// </summary>
+    /// <param name="ts"></param>
+    /// <param name="howMany"></param>
+    /// <returns></returns>
+    public static TimeSpan AddDays(
+        this TimeSpan ts,
+        double howMany
+    )
+    {
+        return ts.Add(
+            TimeSpan.FromDays(howMany)
+        );
+    }
+
+    /// <summary>
+    /// Add this many days to the TimeSpan,
+    /// returning the new value
+    /// </summary>
+    /// <param name="ts"></param>
+    /// <param name="howMany"></param>
+    /// <returns></returns>
+    public static TimeSpan AddHours(
+        this TimeSpan ts,
+        double howMany
+    )
+    {
+        return ts.Add(
+            TimeSpan.FromHours(howMany)
+        );
+    }
+
+    /// <summary>
+    /// Add this many days to the TimeSpan,
+    /// returning the new value
+    /// </summary>
+    /// <param name="ts"></param>
+    /// <param name="howMany"></param>
+    /// <returns></returns>
+    public static TimeSpan AddMinutes(
+        this TimeSpan ts,
+        double howMany
+    )
+    {
+        return ts.Add(
+            TimeSpan.FromMinutes(howMany)
+        );
+    }
+
+    /// <summary>
+    /// Add this many days to the TimeSpan,
+    /// returning the new value
+    /// </summary>
+    /// <param name="ts"></param>
+    /// <param name="howMany"></param>
+    /// <returns></returns>
+    public static TimeSpan AddSeconds(
+        this TimeSpan ts,
+        double howMany
+    )
+    {
+        return ts.Add(
+            TimeSpan.FromSeconds(howMany)
+        );
+    }
+
+    /// <summary>
+    /// Add this many days to the TimeSpan,
+    /// returning the new value
+    /// </summary>
+    /// <param name="ts"></param>
+    /// <param name="howMany"></param>
+    /// <returns></returns>
+    public static TimeSpan AddMilliseconds(
+        this TimeSpan ts,
+        double howMany
+    )
+    {
+        return ts.Add(
+            TimeSpan.FromMilliseconds(howMany)
+        );
+    }
+
+    // TODO
+    // public static TimeSpan TruncateMicroseconds(
+    //     this TimeSpan timeSpan
+    // )
+    // {
+    // }
+
+    /// <summary>
+    /// Returns a new TimeSpan equal to the input with truncated milliseconds
+    /// </summary>
+    /// <param name="timeSpan"></param>
+    /// <returns></returns>
+    public static TimeSpan TruncateMilliseconds(
+        this TimeSpan timeSpan
+    )
+    {
+        return new TimeSpan(
+            timeSpan.Days,
+            timeSpan.Hours,
+            timeSpan.Minutes,
+            timeSpan.Seconds,
+            0
+        );
+    }
+
+    /// <summary>
+    /// Returns a new TimeSpan equal to the input with truncated milliseconds
+    /// </summary>
+    /// <param name="timeSpan"></param>
+    /// <returns></returns>
+    public static TimeSpan TruncateSeconds(
+        this TimeSpan timeSpan
+    )
+    {
+        return new TimeSpan(
+            timeSpan.Days,
+            timeSpan.Hours,
+            timeSpan.Minutes,
+            0,
+            0
+        );
+    }
+
+    /// <summary>
+    /// Returns a new TimeSpan equal to the input with truncated milliseconds
+    /// </summary>
+    /// <param name="timeSpan"></param>
+    /// <returns></returns>
+    public static TimeSpan TruncateMinutes(
+        this TimeSpan timeSpan
+    )
+    {
+        return new TimeSpan(
+            timeSpan.Days,
+            timeSpan.Hours,
+            0,
+            0,
+            0
+        );
+    }
+
+    /// <summary>
+    /// Returns a new TimeSpan equal to the input with truncated milliseconds
+    /// </summary>
+    /// <param name="timeSpan"></param>
+    /// <returns></returns>
+    public static TimeSpan TruncateHours(
+        this TimeSpan timeSpan
+    )
+    {
+        return new TimeSpan(
+            timeSpan.Days,
+            0,
+            0,
+            0,
+            0
+        );
+    }
+}
