042db9403292723566e7361c0aeea89210e41286
Davyd McColl <davydm@gmail.com>
2024-04-17 12:14:48 +0200

    :poop: the missing files...

---
+++ source/TestUtils/PeanutButter.TestUtils.AspNetCore.Tests/Fakes/TestJsonResultExecutor.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using PeanutButter.TestUtils.AspNetCore.Builders;
+using PeanutButter.TestUtils.AspNetCore.Fakes;
+using PeanutButter.Utils;
+using JsonResult = Microsoft.AspNetCore.Mvc.JsonResult;
+using TempDataDictionary = Microsoft.AspNetCore.Mvc.ViewFeatures.TempDataDictionary;
+using ViewResult = Microsoft.AspNetCore.Mvc.ViewResult;
+
+namespace PeanutButter.TestUtils.AspNetCore.Tests.Fakes;
+
+[TestFixture]
+public class TestJsonResultExecutor
+{
+    [Test]
+    public async Task ShouldRenderJsonToTheContext()
+    {
+        // Arrange
+        var actionContext = ActionContextBuilder.BuildDefault();
+        var data = new
+        {
+            id = 1,
+            name = GetRandomString()
+        };
+        var expected = JsonSerializer.Serialize(data);
+        var actionResult = new JsonResult(data);
+        var sut = Create();
+        // Act
+        await sut.ExecuteAsync(
+            actionContext,
+            actionResult
+        );
+        // Assert
+        var result = await actionContext.HttpContext.Response.Body.ReadAllTextAsync();
+        Expect(result)
+            .To.Equal(expected);
+    }
+
+
+    private static IActionResultExecutor<JsonResult> Create()
+    {
+        return new FakeJsonResultExecutor();
+    }
+}
+
+[TestFixture]
+public class TestFakeObjectResultExecutor
+{
+    [Test]
+    public async Task ShouldRenderJsonToTheContext()
+    {
+        // Arrange
+        var actionContext = ActionContextBuilder.BuildDefault();
+        var data = new
+        {
+            id = 1,
+            name = GetRandomString()
+        };
+        var expected = JsonSerializer.Serialize(data);
+        var actionResult = new ObjectResult(data);
+        var sut = Create();
+        // Act
+        await sut.ExecuteAsync(
+            actionContext,
+            actionResult
+        );
+        // Assert
+        var result = await actionContext.HttpContext.Response.Body.ReadAllTextAsync();
+        Expect(result)
+            .To.Equal(expected);
+    }
+
+
+    private static IActionResultExecutor<ObjectResult> Create()
+    {
+        return new FakeObjectResultExecutor();
+    }
+}
+
+[TestFixture]
+public class TestFakeViewResultExecutor
+{
+    [TestFixture]
+    public class ActingOnViewResult
+    {
+        [Test]
+        public async Task ShouldRenderSomethingToTheContext()
+        {
+            // Arrange
+            var actionContext = ActionContextBuilder.BuildDefault();
+            var data = new
+            {
+                id = 1,
+                value = false
+            };
+            var viewName = GetRandomString();
+            var viewDataDictionary = ViewDataDictionaryBuilder.Create()
+                .WithModel(data)
+                .Build();
+            var httpStatusCode = GetRandom<HttpStatusCode>();
+            var contentType = GetRandomFrom(["text/html", "application/json", "text/plain", "text/xml"]);
+            var actionResult = new ViewResult()
+            {
+                ContentType = contentType,
+                StatusCode = (int)httpStatusCode,
+                TempData = new TempDataDictionary(actionContext.HttpContext, new FakeTempDataProvider()),
+                ViewData = viewDataDictionary,
+                ViewEngine = Substitute.For<IViewEngine>(),
+                ViewName = viewName
+            };
+            var sut = Create();
+            // Act
+            await sut.ExecuteAsync(actionContext, actionResult);
+            // Assert
+            var result = await actionContext.HttpContext.Response.Body.ReadAllTextAsync();
+            var opts = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+            Expect(result)
+                .To.Contain($"Status: {actionResult.StatusCode}")
+                .Then($"Content Type: {contentType}")
+                .Then($"View: {viewName}")
+                .Then("Model:")
+                .Then(JsonSerializer.Serialize(data, opts))
+                .Then("ViewData:")
+                .Then(JsonSerializer.Serialize(viewDataDictionary, opts));
+            var httpContext = actionContext.HttpContext;
+            Expect(httpContext.Items["Model"])
+                .To.Be(data);
+            Expect(httpContext.Items["ViewData"])
+                .To.Be(viewDataDictionary);
+            Expect(httpContext.Response.StatusCode)
+                .To.Equal(httpStatusCode);
+            Expect(httpContext.Response.ContentType)
+                .To.Equal(contentType);
+        }
+
+        private static IActionResultExecutor<ViewResult> Create()
+        {
+            return new FakeViewResultExecutor();
+        }
+    }
+
+    [TestFixture]
+    public class ActingOnPartialViewResult
+    {
+        [Test]
+        public async Task ShouldRenderSomethingToTheContext()
+        {
+            // Arrange
+            var actionContext = ActionContextBuilder.BuildDefault();
+            var data = new
+            {
+                id = 1,
+                value = false
+            };
+            var viewName = GetRandomString();
+            var viewDataDictionary = ViewDataDictionaryBuilder.Create()
+                .WithModel(data)
+                .Build();
+            var httpStatusCode = GetRandom<HttpStatusCode>();
+            var contentType = GetRandomFrom(["text/html", "application/json", "text/plain", "text/xml"]);
+            var actionResult = new PartialViewResult()
+            {
+                ContentType = contentType,
+                StatusCode = (int)httpStatusCode,
+                TempData = new TempDataDictionary(actionContext.HttpContext, new FakeTempDataProvider()),
+                ViewData = viewDataDictionary,
+                ViewEngine = Substitute.For<IViewEngine>(),
+                ViewName = viewName
+            };
+            var sut = Create();
+            // Act
+            await sut.ExecuteAsync(actionContext, actionResult);
+            // Assert
+            var result = await actionContext.HttpContext.Response.Body.ReadAllTextAsync();
+            var opts = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+            Expect(result)
+                .To.Contain($"Status: {actionResult.StatusCode}")
+                .Then($"Content Type: {contentType}")
+                .Then($"View: {viewName}")
+                .Then("Model:")
+                .Then(JsonSerializer.Serialize(data, opts))
+                .Then("ViewData:")
+                .Then(JsonSerializer.Serialize(viewDataDictionary, opts));
+            var httpContext = actionContext.HttpContext;
+            Expect(httpContext.Items["Model"])
+                .To.Be(data);
+            Expect(httpContext.Items["ViewData"])
+                .To.Be(viewDataDictionary);
+            Expect(httpContext.Response.StatusCode)
+                .To.Equal(httpStatusCode);
+            Expect(httpContext.Response.ContentType)
+                .To.Equal(contentType);
+        }
+
+        private static IActionResultExecutor<PartialViewResult> Create()
+        {
+            return new FakeViewResultExecutor();
+        }
+    }
+}
---
+++ source/TestUtils/PeanutButter.TestUtils.AspNetCore/Fakes/FakeJsonResultExecutor.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+#if BUILD_PEANUTBUTTER_INTERNAL
+namespace Imported.PeanutButter.TestUtils.AspNetCore.Fakes;
+#else
+namespace PeanutButter.TestUtils.AspNetCore.Fakes;
+#endif
+
+/// <summary>
+/// 
+/// </summary>
+#if BUILD_PEANUTBUTTER_INTERNAL
+internal
+#else
+public
+#endif
+    class FakeJsonResultExecutor
+    : IActionResultExecutor<JsonResult>
+{
+    /// <inheritdoc />
+    public async Task ExecuteAsync(
+        ActionContext context,
+        JsonResult result
+    )
+    {
+        var json = result.Value is null
+            ? ""
+            : JsonSerializer.Serialize(result.Value);
+        await context.HttpContext.Response.WriteAsync(
+            json
+        );
+        context.HttpContext.Response.StatusCode =
+            result.StatusCode ?? (int)HttpStatusCode.OK;
+        context.HttpContext.Response.ContentType =
+            result.ContentType ?? "application/json";
+    }
+}
