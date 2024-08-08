9db721f314ff56e888951bdfff9a63a2c96fb07e
Davyd McColl <davydm@gmail.com>
2024-08-08 09:55:39 +0200

    :sparkles: add OnDisposed event handler
    - with short-cut to set an action in the constructor

--- source/Utils/PeanutButter.Utils.NetCore.Tests/TestAutoLocker.cs
+++ source/Utils/PeanutButter.Utils.NetCore.Tests/TestAutoLocker.cs
@@ -1,146 +1,25 @@
 ﻿// ReSharper disable ObjectCreationAsStatement
 // ReSharper disable MemberCanBePrivate.Global
 
+// ReSharper disable MemberHidesStaticFromOuterClass
+
 namespace PeanutButter.Utils.Tests;
 
 [TestFixture]
-public class TestAutoLockerFat
+public class TestAutoLocker
 {
-    [Test]
-    public void Construct_WhenGivenNullSemaphore_ShouldThrow()
-    {
-        //---------------Set up test pack-------------------
-
-        //---------------Assert Precondition----------------
-
-        //---------------Execute Test ----------------------
-        Expect(() => new AutoLocker(null as Semaphore))
-            .To.Throw<ArgumentNullException>()
-            .For("semaphore");
-
-        //---------------Test Result -----------------------
-    }
-
-    [Test]
-    public void Construct_WhenGivenNullMutex_ShouldThrow()
-    {
-        //---------------Set up test pack-------------------
-
-        //---------------Assert Precondition----------------
-
-        //---------------Execute Test ----------------------
-        Expect(() => new AutoLocker(null as Mutex))
-            .To.Throw<ArgumentNullException>()
-            .For("mutex");
-
-        //---------------Test Result -----------------------
-    }
-
-    [Test]
-    public void Construct_WhenGivenSemaphore_ShouldNotThrow()
-    {
-        //---------------Set up test pack-------------------
-
-        //---------------Assert Precondition----------------
-
-        //---------------Execute Test ----------------------
-        Expect(() => new AutoLocker(new Semaphore(1, 1)))
-            .Not.To.Throw();
-
-        //---------------Test Result -----------------------
-    }
-
-    [Test]
-    public void Construct_WhenGivenMutex_ShouldNotThrow()
-    {
-        //---------------Set up test pack-------------------
-
-        //---------------Assert Precondition----------------
-
-        //---------------Execute Test ----------------------
-        Expect(() => new AutoLocker(new Mutex()))
-            .Not.To.Throw();
-
-        //---------------Test Result -----------------------
-    }
-
-    [Test]
-    public void Construct_WhenGivenSemaphore_ShouldLockSemaphore()
-    {
-        //---------------Set up test pack-------------------
-        var theLock = new Semaphore(1, 1);
-
-        //---------------Assert Precondition----------------
-
-        //---------------Execute Test ----------------------
-        using (new AutoLocker(theLock))
-        {
-            var gotLockAgain = theLock.WaitOne(1);
-            Expect(gotLockAgain)
-                .To.Be.False();
-        }
-
-        var gotLockAfter = theLock.WaitOne(1);
-        //---------------Test Result -----------------------
-        Expect(gotLockAfter)
-            .To.Be.True();
-        theLock.Release();
-    }
-
-    public class MutexLockTest
-    {
-        public bool GotLock { get; private set; }
-        private readonly Mutex _mutex;
-
-        public MutexLockTest(Mutex mutex)
-        {
-            _mutex = mutex;
-        }
-
-        public void RunInThread()
-        {
-            GotLock = _mutex.WaitOne(1);
-        }
-    }
-
-    [Test]
-    public void Construct_WhenGivenMutex_ShouldLockSemaphore()
-    {
-        //---------------Set up test pack-------------------
-        var theLock = new Mutex();
-
-        //---------------Assert Precondition----------------
-
-        //---------------Execute Test ----------------------
-        using (new AutoLocker(theLock))
-        {
-            var test = new MutexLockTest(theLock);
-            var t = new Thread(test.RunInThread);
-            t.Start();
-            t.Join();
-            Expect(test.GotLock)
-                .To.Be.False();
-        }
-
-        var gotLockAfter = theLock.WaitOne(1);
-        //---------------Test Result -----------------------
-        Expect(gotLockAfter)
-            .To.Be.True();
-        theLock.ReleaseMutex();
-    }
-
     [TestFixture]
-    public class OperatingOnSemaphoreSlim
+    public class OperatingOnSemaphore
     {
         [Test]
-        public void Construct_WhenGivenNullSemaphore_ShouldThrow()
+        public void GivenNullSemaphore_ShouldThrow()
         {
             //---------------Set up test pack-------------------
 
             //---------------Assert Precondition----------------
 
             //---------------Execute Test ----------------------
-            Expect(() => new AutoLocker(null as SemaphoreSlim))
+            Expect(() => new AutoLocker(null as Semaphore))
                 .To.Throw<ArgumentNullException>()
                 .For("semaphore");
 
@@ -148,44 +27,77 @@ public class TestAutoLockerFat
         }
 
         [Test]
-        public void Construct_WhenGivenSemaphore_ShouldNotThrow()
+        public void GivenSemaphore_ShouldNotThrow()
         {
             //---------------Set up test pack-------------------
 
             //---------------Assert Precondition----------------
 
             //---------------Execute Test ----------------------
-            Expect(() => new AutoLocker(new SemaphoreSlim(1, 1)))
+            Expect(() => new AutoLocker(new Semaphore(1, 1)))
                 .Not.To.Throw();
 
             //---------------Test Result -----------------------
         }
 
         [Test]
-        public void Construct_WhenGivenSemaphore_ShouldLockSemaphore()
+        public void GivenSemaphore_ShouldLockSemaphore()
         {
             //---------------Set up test pack-------------------
-            var theLock = new SemaphoreSlim(1, 1);
+            var theLock = new Semaphore(1, 1);
 
             //---------------Assert Precondition----------------
 
             //---------------Execute Test ----------------------
             using (new AutoLocker(theLock))
             {
-                var gotLockAgain = theLock.Wait(1);
+                var gotLockAgain = theLock.WaitOne(1);
                 Expect(gotLockAgain)
                     .To.Be.False();
             }
 
-            var gotLockAfter = theLock.Wait(1);
+            var gotLockAfter = theLock.WaitOne(1);
             //---------------Test Result -----------------------
             Expect(gotLockAfter)
                 .To.Be.True();
             theLock.Release();
         }
+    }
+
+    [TestFixture]
+    public class OperatingOnMutex
+    {
+        [Test]
+        public void GivenNullMutex_ShouldThrow()
+        {
+            //---------------Set up test pack-------------------
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            Expect(() => new AutoLocker(null as Mutex))
+                .To.Throw<ArgumentNullException>()
+                .For("mutex");
+
+            //---------------Test Result -----------------------
+        }
 
         [Test]
-        public void Construct_WhenGivenMutex_ShouldLockSemaphore()
+        public void GivenMutex_ShouldNotThrow()
+        {
+            //---------------Set up test pack-------------------
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            Expect(() => new AutoLocker(new Mutex()))
+                .Not.To.Throw();
+
+            //---------------Test Result -----------------------
+        }
+
+        [Test]
+        public void Construct_WhenGivenMutex_ShouldLock()
         {
             //---------------Set up test pack-------------------
             var theLock = new Mutex();
@@ -208,6 +120,175 @@ public class TestAutoLockerFat
             Expect(gotLockAfter)
                 .To.Be.True();
             theLock.ReleaseMutex();
+        }
+    }
+
+    [TestFixture]
+    public class OperatingOnSemaphoreSlim
+    {
+        [Test]
+        public void GivenNullSemaphore_ShouldThrow()
+        {
+            //---------------Set up test pack-------------------
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            Expect(() => new AutoLocker(null as SemaphoreSlim))
+                .To.Throw<ArgumentNullException>()
+                .For("semaphore");
+
+            //---------------Test Result -----------------------
+        }
+
+        [Test]
+        public void GivenSemaphore_ShouldNotThrow()
+        {
+            //---------------Set up test pack-------------------
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            Expect(() => new AutoLocker(new SemaphoreSlim(1, 1)))
+                .Not.To.Throw();
+
+            //---------------Test Result -----------------------
+        }
+
+        [Test]
+        public void GivenSemaphoreSlim_ShouldLock()
+        {
+            //---------------Set up test pack-------------------
+            var theLock = new SemaphoreSlim(1, 1);
+
+            //---------------Assert Precondition----------------
+
+            //---------------Execute Test ----------------------
+            using (new AutoLocker(theLock))
+            {
+                var gotLockAgain = theLock.Wait(1);
+                Expect(gotLockAgain)
+                    .To.Be.False();
+            }
+
+            var gotLockAfter = theLock.Wait(1);
+            //---------------Test Result -----------------------
+            Expect(gotLockAfter)
+                .To.Be.True();
+            theLock.Release();
+        }
+    }
+
+    [TestFixture]
+    public class Events
+    {
+        [TestFixture]
+        public class WhenDisposing
+        {
+            [TestFixture]
+            public class OperatingOnSemaphore
+            {
+                [Test]
+                public void ShouldRunOnDisposedEvent()
+                {
+                    // Arrange
+                    var lck = new Semaphore(1, 1);
+                    var calls = 0;
+                    // Act
+                    using (var _ = new AutoLocker(lck, () => calls++))
+                    {
+                        Expect(lck.WaitOne(0))
+                            .To.Be.False();
+                    }
+
+                    // Assert
+                    Expect(lck.WaitOne(0))
+                        .To.Be.True();
+                    Expect(calls)
+                        .To.Equal(1);
+                }
+            }
+
+            [TestFixture]
+            public class OperatingOnSemaphoreSlim
+            {
+                [Test]
+                public void ShouldRunOnDisposedEvent()
+                {
+                    // Arrange
+                    var lck = new SemaphoreSlim(1, 1);
+                    var calls = 0;
+                    // Act
+                    using (var _ = new AutoLocker(lck, () => calls++))
+                    {
+                        Expect(lck.Wait(0))
+                            .To.Be.False();
+                    }
+
+                    // Assert
+                    Expect(lck.Wait(0))
+                        .To.Be.True();
+                    Expect(calls)
+                        .To.Equal(1);
+                }
+            }
+
+            [TestFixture]
+            public class OperatingOnMutex
+            {
+                [Test]
+                public void ShouldRunOnDisposedEvent()
+                {
+                    // Arrange
+                    var lck = new Mutex();
+                    var calls = 0;
+                    // Act
+                    using (var _ = new AutoLocker(lck, () => calls++))
+                    {
+                        // windows mutexes only lock _between_ threads
+                        // -> which, trust me, has lead to some
+                        //    interesting bugs in the past, and autolocker
+                        //    only supports Mutex types to be more useful - 
+                        //    I'd always recommend using a SemaphoreSlim(1,1)
+                        //    instead so you can catch deadlocks before they're
+                        //    a mystery; anyhoo, that's why this test runs in a
+                        //    separate thread, in the most lazy way I could think
+                        //    of 😅
+                        var results = Run.InParallel(
+                            () =>
+                                Expect(lck.WaitOne(0))
+                                    .To.Be.False(),
+                            () => { }
+                        );
+                        Expect(results)
+                            .To.Contain.All.Matched.By(
+                                o => o is null
+                            );
+                    }
+
+                    // Assert
+                    Expect(lck.WaitOne(0))
+                        .To.Be.True();
+                    Expect(calls)
+                        .To.Equal(1);
+                }
+            }
+        }
+    }
+
+    public class MutexLockTest
+    {
+        public bool GotLock { get; private set; }
+        private readonly Mutex _mutex;
+
+        public MutexLockTest(Mutex mutex)
+        {
+            _mutex = mutex;
+        }
+
+        public void RunInThread()
+        {
+            GotLock = _mutex.WaitOne(1);
         }
     }
 }
--- source/Utils/PeanutButter.Utils/AutoLocker.cs
+++ source/Utils/PeanutButter.Utils/AutoLocker.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Threading;
 
 #if BUILD_PEANUTBUTTER_INTERNAL
@@ -27,13 +28,32 @@ public
     private SemaphoreSlim _slim;
 
     /// <summary>
+    /// Subscribe to this to be notified when this
+    /// locker is disposed, or pass an action in
+    /// on the constructor
+    /// </summary>
+    public EventHandler OnDisposed { get; set; }
+
+    /// <summary>
     /// Constructs a new AutoLocker, immediately locking the provided Semaphore
     /// </summary>
     /// <param name="semaphore">Semaphore to lock immediately</param>
     /// <exception cref="ArgumentNullException">Throws ArgumentNullException if the provided Semaphore is null</exception>
-    public AutoLocker(Semaphore semaphore)
+    public AutoLocker(Semaphore semaphore) : this(semaphore, null)
+    {
+    }
+
+    /// <summary>
+    /// Constructs a new AutoLocker, immediately locking the provided Semaphore,
+    /// with an OnDisposed event handler
+    /// </summary>
+    /// <param name="semaphore"></param>
+    /// <param name="onDisposed"></param>
+    /// <exception cref="ArgumentNullException"></exception>
+    public AutoLocker(Semaphore semaphore, Action onDisposed)
     {
         _fatty = semaphore ?? throw new ArgumentNullException(nameof(semaphore));
+        SetOnDisposed(onDisposed);
         _fatty.WaitOne();
     }
 
@@ -42,9 +62,21 @@ public
     /// </summary>
     /// <param name="semaphore">SemaphoreSlim to lock immediately</param>
     /// <exception cref="ArgumentNullException">Thrown if the provided SemaphoreSlim is null</exception>
-    public AutoLocker(SemaphoreSlim semaphore)
+    public AutoLocker(SemaphoreSlim semaphore): this(semaphore, null)
+    {
+    }
+
+    /// <summary>
+    /// Constructs a new AutoLocker, immediately locking the provided SemaphoreSlim,
+    /// with an OnDisposed event handler
+    /// </summary>
+    /// <param name="semaphore"></param>
+    /// <param name="onDisposed"></param>
+    /// <exception cref="ArgumentNullException"></exception>
+    public AutoLocker(SemaphoreSlim semaphore, Action onDisposed)
     {
         _slim = semaphore ?? throw new ArgumentNullException(nameof(semaphore));
+        SetOnDisposed(onDisposed);
         _slim.Wait();
     }
 
@@ -53,10 +85,32 @@ public
     /// </summary>
     /// <param name="mutex">Mutex to lock immediately</param>
     /// <exception cref="ArgumentNullException">Thrown if the provided Mutex is null</exception>
-    public AutoLocker(Mutex mutex)
+    public AutoLocker(Mutex mutex): this(mutex, null)
+    {
+    }
+
+    /// <summary>
+    /// Constructs a new AutoLocker, immediately locking the provided Mutex,
+    /// with an OnDisposed event handler
+    /// </summary>
+    /// <param name="mutex">Mutex to lock immediately</param>
+    /// <param name="onDisposed"></param>
+    /// <exception cref="ArgumentNullException">Thrown if the provided Mutex is null</exception>
+    public AutoLocker(Mutex mutex, Action onDisposed)
     {
         _mutex = mutex ?? throw new ArgumentNullException(nameof(mutex));
+        SetOnDisposed(onDisposed);
         _mutex.WaitOne();
+    }
+
+    private void SetOnDisposed(Action onDisposed)
+    {
+        if (onDisposed is null)
+        {
+            return;
+        }
+
+        OnDisposed += (_, _) => onDisposed();
     }
 
     /// <inheritdoc />
@@ -64,22 +118,47 @@ public
     {
         lock (_disposeLock)
         {
-            if (_fatty != null)
+            if (_fatty is not null)
             {
                 _fatty.Release();
                 _fatty = null;
+                TriggerOnDisposed();
                 return;
             }
 
-            if (_slim != null)
+            if (_slim is not null)
             {
                 _slim.Release();
                 _slim = null;
+                TriggerOnDisposed();
                 return;
             }
 
-            _mutex.ReleaseMutex();
-            _mutex = null;
+            if (_mutex is not null)
+            {
+                _mutex.ReleaseMutex();
+                TriggerOnDisposed();
+                _mutex = null;
+            }
+        }
+    }
+
+    private void TriggerOnDisposed()
+    {
+        var handlers = OnDisposed;
+        OnDisposed = null;
+        if (handlers is null)
+        {
+            return;
+        }
+
+        try
+        {
+            handlers.Invoke(this, EventArgs.Empty);
+        }
+        catch (Exception ex)
+        {
+            Trace.WriteLine($"OnDisposed handler error: {ex.Message}");
         }
     }
 }
