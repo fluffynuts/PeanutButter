d33c162d3dd194cca0f484c647540adc7b159f6a
Davyd McColl <davydm@gmail.com>
2025-04-17 10:34:56 +0200

    :sparkles: small class to maintain a running average without storing the values

---
+++ source/Utils/PeanutButter.Utils.Tests/TestRunningAverage.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace PeanutButter.Utils.Tests;
+
+[TestFixture]
+public class TestRunningAverage
+{
+    [TestFixture]
+    public class WithNoValues
+    {
+        [Test]
+        public void ShouldReturnZero()
+        {
+            // Arrange
+            var sut = Create();
+            // Act
+            var result = sut.Average;
+            // Assert
+            Expect(result)
+                .To.Equal(0);
+        }
+    }
+
+    [TestFixture]
+    public class WithOneValue
+    {
+        [Test]
+        public void ShouldReturnThatValue()
+        {
+            // Arrange
+            var expected = GetRandomInt(1);
+            var sut = Create();
+            // Act
+            sut.Add(expected);
+            var result = sut.Average;
+            // Assert
+            Expect(result)
+                .To.Equal(expected);
+        }
+    }
+
+    [TestFixture]
+    public class WithNValues
+    {
+        [Test]
+        public void ShouldReturnTheAverage()
+        {
+            // Arrange
+            var values = GetRandomArray<int>(100, 200);
+            var expected = values.Select(o => (decimal)o)
+                .Average();
+            var sut = Create();
+            // Act
+            sut.Add(values.Select(i => (decimal)i).ToArray());
+            var result = sut.Average;
+            // Assert
+            Expect(result)
+                .To.Equal(expected);
+        }
+    }
+
+    private static RunningAverage Create()
+    {
+        return new();
+    }
+}
---
+++ source/Utils/PeanutButter.Utils/RunningAverage.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+
+namespace PeanutButter.Utils;
+
+/// <summary>
+/// Provides a convenience class to keep
+/// a running average of values without
+/// storing the values in memory
+/// </summary>
+public class RunningAverage
+{
+    /// <summary>
+    /// Provides the current average
+    /// </summary>
+    public decimal Average { get; private set; }
+
+    /// <summary>
+    /// Provides a count of items that have been added
+    /// </summary>
+    public int Count { get; private set; }
+
+    /// <summary>
+    /// Add a value to the pool
+    /// - recalculates the average, discards the value
+    /// </summary>
+    /// <param name="value"></param>
+    public void Add(params decimal[] value)
+    {
+        var total = (Count * Average) + value.Sum();
+        Count+= value.Length;
+        Average = total/Count;
+    }
+}
